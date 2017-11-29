9ab9c8cdc3a7e9207999bc4682f7099688f2130c
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2017-11-29 12:04:51 +0000

    CORE-1313 Move ProductGroupWidget to project as an example

---
+++ src/Pyz/Yves/ExampleProductColorGroupWidget/Plugin/ProductWidget/ExampleProductColorGroupWidgetPlugin.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerShop\Yves\ExampleProductColorGroupWidget\Plugin\ProductWidget;
+
+use Spryker\Yves\Kernel\Widget\AbstractWidgetPlugin;
+use SprykerShop\Yves\ProductWidget\Dependency\Plugin\ProductGroupWidget\ProductGroupWidgetPluginInterface;
+
+class ExampleProductColorGroupWidgetPlugin extends AbstractWidgetPlugin implements ProductGroupWidgetPluginInterface
+{
+
+    /**
+     * @param int $idProductAbstract
+     *
+     * @return void
+     */
+    public function initialize(int $idProductAbstract): void
+    {
+        $this->addParameter('idProductAbstract', $idProductAbstract);
+    }
+
+    /**
+     * @return string
+     */
+    public static function getName(): string
+    {
+        return static::NAME;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTemplate(): string
+    {
+        return '@ExampleProductColorGroupWidget/_product-widget/product-groups.twig';
+    }
+
+}
