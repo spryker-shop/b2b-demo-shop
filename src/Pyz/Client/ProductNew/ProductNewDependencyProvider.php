596ef8e3e0445a6006f4efb7808b51bd5ee42eaf
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-11-29 12:46:06 +0100

    CORE-1313 Moved ProductNew client from shop to core

---
+++ src/Pyz/Client/ProductNew/ProductNewDependencyProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Client\ProductNew;
+
+use Spryker\Client\Catalog\Plugin\Elasticsearch\ResultFormatter\RawCatalogSearchResultFormatterPlugin;
+use Spryker\Client\ProductNew\ProductNewDependencyProvider as SprykerProductNewDependencyProvider;
+use Spryker\Client\Search\Plugin\Elasticsearch\QueryExpander\FacetQueryExpanderPlugin;
+use Spryker\Client\Search\Plugin\Elasticsearch\QueryExpander\LocalizedQueryExpanderPlugin;
+use Spryker\Client\Search\Plugin\Elasticsearch\QueryExpander\PaginatedQueryExpanderPlugin;
+use Spryker\Client\Search\Plugin\Elasticsearch\QueryExpander\SortedQueryExpanderPlugin;
+use Spryker\Client\Search\Plugin\Elasticsearch\QueryExpander\StoreQueryExpanderPlugin;
+use Spryker\Client\Search\Plugin\Elasticsearch\ResultFormatter\FacetResultFormatterPlugin;
+use Spryker\Client\Search\Plugin\Elasticsearch\ResultFormatter\PaginatedResultFormatterPlugin;
+use Spryker\Client\Search\Plugin\Elasticsearch\ResultFormatter\SortedResultFormatterPlugin;
+
+class ProductNewDependencyProvider extends SprykerProductNewDependencyProvider
+{
+    /**
+     * @return \Spryker\Client\Search\Dependency\Plugin\QueryExpanderPluginInterface[]
+     */
+    protected function getNewProductsQueryExpanderPlugins()
+    {
+        return [
+            new StoreQueryExpanderPlugin(),
+            new LocalizedQueryExpanderPlugin(),
+            new FacetQueryExpanderPlugin(),
+            new SortedQueryExpanderPlugin(),
+            new PaginatedQueryExpanderPlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Client\Search\Dependency\Plugin\ResultFormatterPluginInterface[]
+     */
+    protected function getNewProductsResultFormatterPlugins()
+    {
+        return [
+            new FacetResultFormatterPlugin(),
+            new SortedResultFormatterPlugin(),
+            new PaginatedResultFormatterPlugin(),
+            new RawCatalogSearchResultFormatterPlugin(),
+        ];
+    }
+}
