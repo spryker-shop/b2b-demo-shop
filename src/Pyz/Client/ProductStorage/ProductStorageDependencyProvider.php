a15f2a0476afe23138ee06babf451280b247e6d9
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-12-19 11:20:27 +0100

    CORE-1313 Reading product storage data provided by Publish and Sync

---
+++ src/Pyz/Client/ProductStorage/ProductStorageDependencyProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Client\ProductStorage;
+
+use Spryker\Client\AvailabilityStorage\Plugin\StorageProductAvailabilityStorageExpanderPlugin;
+use Spryker\Client\PriceStorage\Plugin\ProductViewPriceExpanderPlugin;
+use Spryker\Client\ProductImageStorage\Plugin\ProductViewImageExpanderPlugin;
+use Spryker\Client\ProductStorage\Plugin\ProductViewVariantExpanderPlugin;
+use Spryker\Client\ProductStorage\ProductStorageDependencyProvider as SprykerProductStorageDependencyProvider;
+
+class ProductStorageDependencyProvider extends SprykerProductStorageDependencyProvider
+{
+    /**
+     * @return \Spryker\Client\ProductStorage\Dependency\Plugin\ProductViewExpanderPluginInterface[]
+     */
+    protected function getProductViewExpanderPlugins()
+    {
+        return [
+            new ProductViewVariantExpanderPlugin(),
+            new ProductViewPriceExpanderPlugin(),
+            new StorageProductAvailabilityStorageExpanderPlugin(),
+            new ProductViewImageExpanderPlugin(),
+        ];
+    }
+}
