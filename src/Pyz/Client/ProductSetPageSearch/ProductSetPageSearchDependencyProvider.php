a8919a076a7c3cede5f4bb08f655b9fc56afe30e
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2017-12-28 11:59:06 +0000

    CORE-1313 Add ProductSetStorage

--- src/Pyz/Client/ProductSetPageSearch/ProductSetPageSearchDependencyProvider.php
+++ src/Pyz/Client/ProductSetPageSearch/ProductSetPageSearchDependencyProvider.php
@@ -1,26 +1,22 @@
 <?php
 
-/**
- * This file is part of the Spryker Demoshop.
- * For full license information, please view the LICENSE file that was distributed with this source code.
- */
+namespace Pyz\Client\ProductSetPageSearch;
 
-namespace Pyz\Client\ProductSet;
-
-use Spryker\Client\ProductSet\Plugin\Elasticsearch\ResultFormatter\ProductSetListResultFormatterPlugin;
-use Spryker\Client\ProductSet\ProductSetDependencyProvider as SprykerProductSetDependencyProvider;
+use Spryker\Client\ProductSetPageSearch\Plugin\Elasticsearch\ResultFormatter\ProductSetPageSearchListResultFormatterPlugin;
+use Spryker\Client\ProductSetPageSearch\ProductSetPageSearchDependencyProvider as SprykerProductSetPageSearchDependencyProvider;
 use Spryker\Client\Search\Plugin\Elasticsearch\QueryExpander\LocalizedQueryExpanderPlugin;
 use Spryker\Client\Search\Plugin\Elasticsearch\QueryExpander\StoreQueryExpanderPlugin;
 
-class ProductSetDependencyProvider extends SprykerProductSetDependencyProvider
+class ProductSetPageSearchDependencyProvider extends SprykerProductSetPageSearchDependencyProvider
 {
+
     /**
      * @return array
      */
     protected function getProductSetListResultFormatterPlugins()
     {
         return [
-            new ProductSetListResultFormatterPlugin(),
+            new ProductSetPageSearchListResultFormatterPlugin(),
         ];
     }
 
