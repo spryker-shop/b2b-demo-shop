2cb7e846fe0bb5cc77ca53eb4d6c2e6dc70ba75d
Denis Turkov <denis.turkov@spryker.com>
2018-01-12 17:23:49 +0100

    CORE-974: Update phpdoc

--- src/Pyz/Yves/ShopPermission/ShopPermissionDependencyProvider.php
+++ src/Pyz/Yves/ShopPermission/ShopPermissionDependencyProvider.php
@@ -6,6 +6,9 @@ use SprykerShop\Yves\ShopPermission\Plugin\Twig\PermissionTwigFunctionPlugin;
 
 class ShopPermissionDependencyProvider extends \SprykerShop\Yves\ShopPermission\ShopPermissionDependencyProvider
 {
+    /**
+     * @return \Spryker\Yves\Twig\Plugin\TwigFunctionPluginInterface[]
+     */
     protected function getPermissionTwigFunctionPlugins()
     {
         return [
