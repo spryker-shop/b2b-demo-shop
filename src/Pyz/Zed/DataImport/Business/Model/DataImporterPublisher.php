b2a5e35d39af6602aee07e08967d847b9b13dedd
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2018-01-16 10:12:07 +0000

    Fix data importer

--- src/Pyz/Zed/DataImport/Business/Model/DataImporterPublisher.php
+++ src/Pyz/Zed/DataImport/Business/Model/DataImporterPublisher.php
@@ -8,6 +8,7 @@
 namespace Pyz\Zed\DataImport\Business\Model;
 
 use Generated\Shared\Transfer\EventEntityTransfer;
+use Spryker\Zed\Event\Business\EventFacadeInterface;
 
 class DataImporterPublisher implements DataImporterPublisherInterface
 {
