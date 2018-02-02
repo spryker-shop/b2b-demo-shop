e1bde11bc94d3974908a0307f260218c2b1a694b
Tamas Nyulas <tamas.nyulas@spryker.com>
2018-02-02 13:05:22 +0100

    CORE-1569 Adjusted travis Elasticsearch installation

--- config/Shared/ci/travis/install_elasticsearch.sh
+++ config/Shared/ci/travis/install_elasticsearch.sh
@@ -2,4 +2,4 @@
 
 mkdir /home/travis/elasticsearch
 wget -O - https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.6.7.tar.gz | tar xz --directory=/home/travis/elasticsearch --strip-components=1
-/home/travis/elasticsearch/bin/elasticsearch --daemonize --path.data /home/travis/elasticsearch
+/home/travis/elasticsearch/bin/elasticsearch --daemonize
