9e2a3b790a2c9509e494702725ca1b3dabf894a7
Karoly Gerner <karoly.gerner@spryker.com>
2018-02-02 14:36:06 +0000

    CORE-1759 Fixing argument type, updating composer

--- src/Pyz/Client/Quote/QuoteDependencyProvider.php
+++ src/Pyz/Client/Quote/QuoteDependencyProvider.php
@@ -7,6 +7,7 @@
 
 namespace Pyz\Client\Quote;
 
+use Spryker\Client\Kernel\Container;
 use Spryker\Client\Quote\QuoteDependencyProvider as BaseQuoteDependencyProvider;
 use Spryker\Client\Store\Plugin\StoreQuoteTransferExpanderPlugin;
 
@@ -17,7 +18,7 @@ class QuoteDependencyProvider extends BaseQuoteDependencyProvider
      *
      * @return \Spryker\Client\Quote\Dependency\Plugin\QuoteTransferExpanderPluginInterface[]
      */
-    protected function getQuoteTransferExpanderPlugins($container)
+    protected function getQuoteTransferExpanderPlugins(Container $container)
     {
         return [
             new StoreQuoteTransferExpanderPlugin(),
