ac97a8d1ba72142b36c69b06a750ef381fff34da
ilmente <alessandro.bellini@spryker.com>
2018-02-07 11:44:48 +0100

    CORE-2260: removes dummy/test components

--- src/Pyz/Yves/ShopUI/Theme/default/components/atoms/another-component/index.ts
+++
@@ -1,3 +0,0 @@
-import './another-component.scss';
-import { register } from 'ShopUI/app';
-export default register('another-component', () => import(/* webpackMode: "lazy" */'./another-component'));
--- src/Pyz/Yves/ShopUI/Theme/default/components/atoms/test-component/index.ts
+++
@@ -1,3 +0,0 @@
-import './test-component.scss';
-import { register } from 'ShopUI/app';
-export default register('test-component', () => import(/* webpackMode: "lazy" */'./test-component'));
