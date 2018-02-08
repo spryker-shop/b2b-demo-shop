df9387440c71191bea7ca7e2813ec94542776e56
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2018-02-08 16:55:41 +0000

    CORE-3235 Fix travis

--- src/Pyz/Yves/ShopPermission/ShopPermissionDependencyProvider.php
+++ src/Pyz/Yves/ShopPermission/ShopPermissionDependencyProvider.php
@@ -1,10 +1,16 @@
 <?php
 
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Pyz\Yves\ShopPermission;
 
 use SprykerShop\Yves\ShopPermission\Plugin\Twig\PermissionTwigFunctionPlugin;
+use SprykerShop\Yves\ShopPermission\ShopPermissionDependencyProvider as SprykerShopShopPermissionDependencyProvider;
 
-class ShopPermissionDependencyProvider extends \SprykerShop\Yves\ShopPermission\ShopPermissionDependencyProvider
+class ShopPermissionDependencyProvider extends SprykerShopShopPermissionDependencyProvider
 {
     /**
      * @return \Spryker\Yves\Twig\Plugin\TwigFunctionPluginInterface[]
@@ -12,7 +18,7 @@ class ShopPermissionDependencyProvider extends \SprykerShop\Yves\ShopPermission\
     protected function getPermissionTwigFunctionPlugins()
     {
         return [
-            new PermissionTwigFunctionPlugin()
+            new PermissionTwigFunctionPlugin(),
         ];
     }
 }
