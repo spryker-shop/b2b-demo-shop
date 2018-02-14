4cafaac2d3f8859edd8af399c6a35148bdad8cd5
vseselov <alexander.veselov@spryker.com>
2018-02-14 12:00:09 +0200

    CORE-2413: dataset module

---
+++ src/Orm/Zed/Dataset/Persistence/SpyDatasetRowColValue.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\Dataset\Persistence;
+
+use Spryker\Zed\Dataset\Persistence\Propel\AbstractSpyDatasetRowColValue as BaseSpyDatasetRowColValue;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_dataset_row_col_value' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyDatasetRowColValue extends BaseSpyDatasetRowColValue
+{
+
+}
---
+++ src/Orm/Zed/Dataset/Persistence/SpyDatasetRowColumnValueQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\Dataset\Persistence;
+
+use Spryker\Zed\Dataset\Persistence\Propel\AbstractSpyDatasetRowColumnValueQuery as BaseSpyDatasetRowColumnValueQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_dataset_row_column_value' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyDatasetRowColumnValueQuery extends BaseSpyDatasetRowColumnValueQuery
+{
+
+}
