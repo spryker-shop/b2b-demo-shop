4eaf3ebd468e7c3de6bb2fddc194507ce6b1acf8
Vasiliy Migel <vasiliy.migel@spryker.com>
2018-02-15 20:28:50 +0200

    CORE-2414 fix code after CR

--- src/Orm/Zed/FileManager/Persistence/SpyFileQuery.php
+++ src/Orm/Zed/FileManager/Persistence/SpyFileQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\FileManager\Persistence;
 
 use Spryker\Zed\FileManager\Persistence\Propel\AbstractSpyFileQuery as BaseSpyFileQuery;
@@ -10,11 +15,10 @@ use Spryker\Zed\FileManager\Persistence\Propel\AbstractSpyFileQuery as BaseSpyFi
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyFileQuery extends BaseSpyFileQuery
 {
-
 }
