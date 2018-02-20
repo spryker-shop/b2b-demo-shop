0c885052e83f7f9e5e741cda6908aba97cef00be
Denis Turkov <denis.turkov@spryker.com>
2018-02-20 10:13:36 +0000

    CORE-777: Update phpdoc for propel entities

--- src/Orm/Zed/GiftCardBalance/Persistence/SpyGiftCardBalanceLog.php
+++ src/Orm/Zed/GiftCardBalance/Persistence/SpyGiftCardBalanceLog.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\GiftCardBalance\Persistence;
 
 use Orm\Zed\GiftCardBalance\Persistence\Base\SpyGiftCardBalanceLog as BaseSpyGiftCardBalanceLog;
@@ -10,11 +15,10 @@ use Orm\Zed\GiftCardBalance\Persistence\Base\SpyGiftCardBalanceLog as BaseSpyGif
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyGiftCardBalanceLog extends BaseSpyGiftCardBalanceLog
 {
-
 }
--- src/Orm/Zed/GiftCardBalance/Persistence/SpyGiftCardBalanceLogQuery.php
+++ src/Orm/Zed/GiftCardBalance/Persistence/SpyGiftCardBalanceLogQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\GiftCardBalance\Persistence;
 
 use Orm\Zed\GiftCardBalance\Persistence\Base\SpyGiftCardBalanceLogQuery as BaseSpyGiftCardBalanceLogQuery;
@@ -10,11 +15,10 @@ use Orm\Zed\GiftCardBalance\Persistence\Base\SpyGiftCardBalanceLogQuery as BaseS
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyGiftCardBalanceLogQuery extends BaseSpyGiftCardBalanceLogQuery
 {
-
 }
