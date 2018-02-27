401355f91a6161ba35c46e451266e6ef0b968a5e
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2018-02-27 16:35:26 +0200

    CORE-2347: Added quote sync plugin to customer dependecy

--- src/Pyz/Client/Customer/CustomerDependencyProvider.php
+++ src/Pyz/Client/Customer/CustomerDependencyProvider.php
@@ -11,6 +11,7 @@ use Spryker\Client\Cart\Plugin\CustomerChangeCartUpdatePlugin;
 use Spryker\Client\Customer\CustomerDependencyProvider as SprykerCustomerDependencyProvider;
 use Spryker\Client\Customer\Plugin\CustomerAddressSessionUpdatePlugin;
 use Spryker\Client\Customer\Plugin\CustomerTransferRefreshPlugin;
+use Spryker\Client\Quote\Plugin\CustomerChangeQuoteSyncPlugin;
 
 class CustomerDependencyProvider extends SprykerCustomerDependencyProvider
 {
@@ -30,6 +31,7 @@ class CustomerDependencyProvider extends SprykerCustomerDependencyProvider
     protected function getCustomerSessionSetPlugins()
     {
         return [
+            new CustomerChangeQuoteSyncPlugin(),
             new CustomerChangeCartUpdatePlugin(),
         ];
     }
