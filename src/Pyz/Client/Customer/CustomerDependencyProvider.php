720db6ee07e2f4b8a151f5e1fca8c47acdec5835
Tamas Nyulas <tamas.nyulas@spryker.com>
2018-03-13 16:29:02 +0000

    CORE-2347 Minor refactoring

--- src/Pyz/Client/Customer/CustomerDependencyProvider.php
+++ src/Pyz/Client/Customer/CustomerDependencyProvider.php
@@ -11,7 +11,7 @@ use Spryker\Client\Cart\Plugin\CustomerChangeCartUpdatePlugin;
 use Spryker\Client\Customer\CustomerDependencyProvider as SprykerCustomerDependencyProvider;
 use Spryker\Client\Customer\Plugin\CustomerAddressSessionUpdatePlugin;
 use Spryker\Client\Customer\Plugin\CustomerTransferRefreshPlugin;
-use Spryker\Client\PersistentCart\Plugin\CustomerChangeQuoteMergePlugin;
+use Spryker\Client\PersistentCart\Plugin\GuestCartUpdateCustomerSessionSetPlugin;
 
 class CustomerDependencyProvider extends SprykerCustomerDependencyProvider
 {
@@ -31,7 +31,7 @@ class CustomerDependencyProvider extends SprykerCustomerDependencyProvider
     protected function getCustomerSessionSetPlugins()
     {
         return [
-            new CustomerChangeQuoteMergePlugin(),
+            new GuestCartUpdateCustomerSessionSetPlugin(),
             new CustomerChangeCartUpdatePlugin(),
         ];
     }
