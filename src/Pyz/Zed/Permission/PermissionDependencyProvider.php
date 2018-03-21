01b3785f5a74af5bdddefbecdcfc45989f412035
Tamas Nyulas <tamas.nyulas@spryker.com>
2018-03-21 10:12:30 +0100

    CORE-2347 Added shared cart permission installer

--- src/Pyz/Zed/Permission/PermissionDependencyProvider.php
+++ src/Pyz/Zed/Permission/PermissionDependencyProvider.php
@@ -11,7 +11,7 @@ use Spryker\Zed\CompanyRole\Communication\Plugin\PermissionStoragePlugin;
 use Spryker\Zed\Permission\PermissionDependencyProvider as SprykerPermissionDependencyProvider;
 use Spryker\Zed\SharedCart\Communication\Plugin\QuotePermissionStoragePlugin;
 use Spryker\Zed\SharedCart\Communication\Plugin\ReadSharedCartPermissionPlugin;
-use Spryker\Zed\SharedCart\Communication\Plugin\WriteharedCartPermissionPlugin;
+use Spryker\Zed\SharedCart\Communication\Plugin\WriteSharedCartPermissionPlugin;
 
 class PermissionDependencyProvider extends SprykerPermissionDependencyProvider
 {
@@ -33,7 +33,7 @@ class PermissionDependencyProvider extends SprykerPermissionDependencyProvider
     {
         return [
             new ReadSharedCartPermissionPlugin(),
-            new WriteharedCartPermissionPlugin(),
+            new WriteSharedCartPermissionPlugin(),
         ];
     }
 }
