8109fa6e9fa4cd42fd7bd00eaffa2f59cbf561d3
khatsko <kirill.khatsko@spryker.com>
2018-03-29 19:52:19 +0300

    CORE-3338 add orm

---
+++ src/Orm/Zed/SalesReclamation/Persistence/SpySalesReclamationItem.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\SalesReclamation\Persistence;
+
+use Orm\Zed\SalesReclamation\Persistence\Base\SpySalesReclamationItem as BaseSpySalesReclamationItem;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_sales_reclamation_item' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpySalesReclamationItem extends BaseSpySalesReclamationItem
+{
+
+}
