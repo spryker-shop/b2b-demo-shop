97acd8af6ef396904c415b014bfc7b73101d8dd8
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2018-04-05 17:27:11 +0000

    core-2347 fixed cs problems

--- src/Orm/Zed/ShoppingList/Persistence/SpyShoppingListCompanyBusinessUnitQuery.php
+++ src/Orm/Zed/ShoppingList/Persistence/SpyShoppingListCompanyBusinessUnitQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\ShoppingList\Persistence;
 
 use Orm\Zed\ShoppingList\Persistence\Base\SpyShoppingListCompanyBusinessUnitQuery as BaseSpyShoppingListCompanyBusinessUnitQuery;
@@ -10,11 +15,10 @@ use Orm\Zed\ShoppingList\Persistence\Base\SpyShoppingListCompanyBusinessUnitQuer
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyShoppingListCompanyBusinessUnitQuery extends BaseSpyShoppingListCompanyBusinessUnitQuery
 {
-
 }
