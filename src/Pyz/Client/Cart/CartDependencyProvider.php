fcdd49b7e15b98dc50232877ac7a855f37ba091c
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2018-04-06 12:05:11 +0300

    CORE-2347: Code refactoring

--- src/Pyz/Client/Cart/CartDependencyProvider.php
+++ src/Pyz/Client/Cart/CartDependencyProvider.php
@@ -10,8 +10,8 @@ namespace Pyz\Client\Cart;
 use Spryker\Client\Cart\CartDependencyProvider as SprykerCartDependencyProvider;
 use Spryker\Client\DiscountPromotion\Plugin\AddDiscountPromotionCartRequestExpandPlugin;
 use Spryker\Client\PersistentCart\Plugin\DatabaseQuoteStorageStrategy;
-use Spryker\Client\ProductBundle\Plugin\Cart\BundledItemCountPlugin;
 use Spryker\Client\ProductBundle\Plugin\Cart\BundleProductQuoteItemFinderPlugin;
+use Spryker\Client\ProductBundle\Plugin\Cart\ItemCountPlugin;
 use Spryker\Client\ProductBundle\Plugin\Cart\RemoveBundleChangeRequestExpanderPlugin;
 
 class CartDependencyProvider extends SprykerCartDependencyProvider
@@ -21,7 +21,7 @@ class CartDependencyProvider extends SprykerCartDependencyProvider
      */
     protected function getItemCountPlugin()
     {
-        return new BundledItemCountPlugin();
+        return new ItemCountPlugin();
     }
 
     /**
@@ -44,7 +44,7 @@ class CartDependencyProvider extends SprykerCartDependencyProvider
     }
 
     /**
-     * @return \Spryker\Client\Cart\Dependency\Plugin\CartChangeRequestExpanderPluginInterface[]
+     * @return \Spryker\Client\CartExtension\Dependency\Plugin\CartChangeRequestExpanderPluginInterface[]
      */
     protected function getAddItemsRequestExpanderPlugins()
     {
@@ -54,7 +54,7 @@ class CartDependencyProvider extends SprykerCartDependencyProvider
     }
 
     /**
-     * @return \Spryker\Client\Cart\Dependency\Plugin\CartChangeRequestExpanderPluginInterface[]
+     * @return \Spryker\Client\CartExtension\Dependency\Plugin\CartChangeRequestExpanderPluginInterface[]
      */
     protected function getRemoveItemsRequestExpanderPlugins()
     {
