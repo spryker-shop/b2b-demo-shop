ee4274df1a4c9e40222d53ad31396fda068fd579
Artem Kuznetsov <wizardphp@gmail.com>
2018-04-07 09:00:32 +0000

    code sniff

--- src/Orm/Zed/ManualOrderEntry/Persistence/SpyOrderSource.php
+++ src/Orm/Zed/ManualOrderEntry/Persistence/SpyOrderSource.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\ManualOrderEntry\Persistence;
 
 use Orm\Zed\ManualOrderEntry\Persistence\Base\SpyOrderSource as BaseSpyOrderSource;
@@ -10,11 +15,10 @@ use Orm\Zed\ManualOrderEntry\Persistence\Base\SpyOrderSource as BaseSpyOrderSour
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyOrderSource extends BaseSpyOrderSource
 {
-
 }
--- src/Orm/Zed/ManualOrderEntry/Persistence/SpyOrderSourceQuery.php
+++ src/Orm/Zed/ManualOrderEntry/Persistence/SpyOrderSourceQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\ManualOrderEntry\Persistence;
 
 use Orm\Zed\ManualOrderEntry\Persistence\Base\SpyOrderSourceQuery as BaseSpyOrderSourceQuery;
@@ -10,11 +15,10 @@ use Orm\Zed\ManualOrderEntry\Persistence\Base\SpyOrderSourceQuery as BaseSpyOrde
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyOrderSourceQuery extends BaseSpyOrderSourceQuery
 {
-
 }
