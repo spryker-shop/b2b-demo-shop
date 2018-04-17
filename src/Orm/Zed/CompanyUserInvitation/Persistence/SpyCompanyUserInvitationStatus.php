6a39232c2ca48d9f36253698719c07f9a2ee8864
theodoros.liokos@spryker.com <theodoros.liokos@spryker.com>
2018-04-17 11:55:32 +0200

    CORE-2663: Code Sniffer Fixes

--- src/Orm/Zed/CompanyUserInvitation/Persistence/SpyCompanyUserInvitationStatus.php
+++ src/Orm/Zed/CompanyUserInvitation/Persistence/SpyCompanyUserInvitationStatus.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\CompanyUserInvitation\Persistence;
 
 use Orm\Zed\CompanyUserInvitation\Persistence\Base\SpyCompanyUserInvitationStatus as BaseSpyCompanyUserInvitationStatus;
@@ -10,11 +15,10 @@ use Orm\Zed\CompanyUserInvitation\Persistence\Base\SpyCompanyUserInvitationStatu
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyCompanyUserInvitationStatus extends BaseSpyCompanyUserInvitationStatus
 {
-
 }
