7b07b85a082d1933486f7f248ff459e282e0a319
Kyrylo Khatsko <kirill.khatsko@spryker.com>
2018-05-04 09:12:50 +0300

    CORE-2655 Barcode: fix barcode plugin stack

--- src/Pyz/Service/Barcode/BarcodeDependencyProvider.php
+++ src/Pyz/Service/Barcode/BarcodeDependencyProvider.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace Pyz\Zed\Barcode;
+namespace Pyz\Service\Barcode;
 
 use Spryker\Service\Barcode\BarcodeDependencyProvider as SprykerDependencyProvider;
 use Spryker\Service\CodeItNow\Plugin\EANBarcodeGenerator;
