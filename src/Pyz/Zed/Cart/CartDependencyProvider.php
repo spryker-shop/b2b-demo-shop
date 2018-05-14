13e19205591c28d42bdfa8ff6c888fa7d19496a2
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2018-05-14 19:16:34 +0300

    PS-249: Added inactive product quote filter

--- src/Pyz/Zed/Cart/CartDependencyProvider.php
+++ src/Pyz/Zed/Cart/CartDependencyProvider.php
@@ -24,6 +24,7 @@ use Spryker\Zed\ProductBundle\Communication\Plugin\Cart\ExpandBundleItemsPlugin;
 use Spryker\Zed\ProductBundle\Communication\Plugin\Cart\ExpandBundleItemsWithImagesPlugin;
 use Spryker\Zed\ProductCartConnector\Communication\Plugin\ProductCartPlugin;
 use Spryker\Zed\ProductCartConnector\Communication\Plugin\ProductExistsCartPreCheckPlugin;
+use Spryker\Zed\ProductCartConnector\Communication\Plugin\RemoveInactiveItemsPreReloadPlugin;
 use Spryker\Zed\ProductImageCartConnector\Communication\Plugin\ProductImageCartPlugin;
 use Spryker\Zed\ProductMeasurementUnit\Communication\Plugin\Cart\ProductMeasurementSalesUnitGroupKeyItemExpanderPlugin;
 use Spryker\Zed\ProductMeasurementUnit\Communication\Plugin\Cart\ProductMeasurementUnitItemExpanderPlugin;
@@ -106,6 +107,7 @@ class CartDependencyProvider extends SprykerCartDependencyProvider
     {
         return [
            new CartBundleItemsPreReloadPlugin(),
+           new RemoveInactiveItemsPreReloadPlugin(),
         ];
     }
 
