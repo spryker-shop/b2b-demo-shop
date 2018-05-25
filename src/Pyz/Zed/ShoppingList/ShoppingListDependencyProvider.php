586c073a002c866ad7909176284fc461e3a464c4
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2018-05-25 12:19:04 +0300

    PS-249: Code review updates

--- src/Pyz/Zed/ShoppingList/ShoppingListDependencyProvider.php
+++ src/Pyz/Zed/ShoppingList/ShoppingListDependencyProvider.php
@@ -7,18 +7,18 @@
 
 namespace Pyz\Zed\ShoppingList;
 
-use Spryker\Zed\ProductBundle\Communication\Plugin\ShoppingList\ReplaceBundledQuoteItemsExpanderPlugin;
+use Spryker\Zed\ProductBundle\Communication\Plugin\ShoppingList\ReplaceBundledQuoteItemsPreConvertPlugin;
 use Spryker\Zed\ShoppingList\ShoppingListDependencyProvider as SprykerShoppingListDependencyProvider;
 
 class ShoppingListDependencyProvider extends SprykerShoppingListDependencyProvider
 {
     /**
-     * @return \Spryker\Zed\ShoppingListExtension\Dependency\Plugin\QuoteItemsExtractorExpanderPluginInterface[]
+     * @return \Spryker\Zed\ShoppingListExtension\Dependency\Plugin\QuoteItemsPreConvertPluginInterface[]
      */
     protected function getQuoteItemExpanderPlugins(): array
     {
         return [
-            new ReplaceBundledQuoteItemsExpanderPlugin(),
+            new ReplaceBundledQuoteItemsPreConvertPlugin(),
         ];
     }
 }
