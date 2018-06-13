8f2488618603c9781fdcd92350e42bf6cc31bdc9
ilmente <alessandro.bellini@spryker.com>
2018-06-13 11:57:27 +0200

    te-7: add es6-polyfill entry point for old browser compoatibility

--- src/Pyz/Yves/ShopUi/Theme/default/vendor.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/vendor.ts
@@ -1,20 +1,2 @@
-declare function require(moduleName: string): any;
-
-// add es6 polyfill
-import 'core-js/fn/promise';
-import 'core-js/fn/array';
-import 'core-js/fn/set';
-import 'core-js/fn/map';
-
-// HTMLTemplateElement, Event, CustomEvent, MouseEvent, Object.assign, Array.from and URL constructor
-// are polyfilled by webcomponents API
-// https://github.com/webcomponents/webcomponentsjs#webcomponentsjs-v1-spec-polyfills
-
-// if webcomponents are defined as native
-// a es5 compile shim is required
-if (!!window.customElements) {
-    import(/* webpackMode: "eager" */'@webcomponents/webcomponentsjs/custom-elements-es5-adapter');
-}
-
 // add webcomponents polyfill
-require('@webcomponents/webcomponentsjs/webcomponents-bundle');
+import '@webcomponents/webcomponentsjs/webcomponents-bundle';
