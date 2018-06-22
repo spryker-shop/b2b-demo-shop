6646372f669ec1e263925d3178093b8b297ede33
voitovtihran <voitovtihra@gmail.com>
2018-06-22 07:16:36 +0000

    PS-982 Adding of product alternative storage project persistence

---
+++ src/Orm/Zed/ProductAlternativeStorage/Persistence/SpyProductAlternativeStorage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductAlternativeStorage\Persistence;
+
+use Orm\Zed\ProductAlternativeStorage\Persistence\Base\SpyProductAlternativeStorage as BaseSpyProductAlternativeStorage;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_product_alternative_storage' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductAlternativeStorage extends BaseSpyProductAlternativeStorage
+{
+
+}
---
+++ src/Orm/Zed/ProductAlternativeStorage/Persistence/SpyProductAlternativeStorageQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductAlternativeStorage\Persistence;
+
+use Orm\Zed\ProductAlternativeStorage\Persistence\Base\SpyProductAlternativeStorageQuery as BaseSpyProductAlternativeStorageQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_product_alternative_storage' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductAlternativeStorageQuery extends BaseSpyProductAlternativeStorageQuery
+{
+
+}
