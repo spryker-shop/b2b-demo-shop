992e208c292272f1f740b774d7972b209bb89b74
Ruslan Dovhospynyi <dovhospynyi@MacBook-Pro-15-Dovgospiniy.local>
2018-07-10 09:58:53 +0300

    PS-1995 rename plugins according interface names

--- src/Pyz/Zed/EventBehavior/EventBehaviorDependencyProvider.php
+++ src/Pyz/Zed/EventBehavior/EventBehaviorDependencyProvider.php
@@ -7,86 +7,86 @@
 
 namespace Pyz\Zed\EventBehavior;
 
-use Spryker\Zed\AvailabilityStorage\Communication\Plugin\Event\AvailabilityEventResourcePlugin;
-use Spryker\Zed\CategoryPageSearch\Communication\Plugin\Event\CategoryPageEventResourcePlugin;
-use Spryker\Zed\CategoryStorage\Communication\Plugin\Event\CategoryNodeEventResourcePlugin;
-use Spryker\Zed\CategoryStorage\Communication\Plugin\Event\CategoryTreeEventResourcePlugin;
-use Spryker\Zed\CmsBlockCategoryStorage\Communication\Plugin\Event\CmsBlockCategoryEventResourcePlugin;
-use Spryker\Zed\CmsBlockProductStorage\Communication\Plugin\Event\CmsBlockProductEventResourcePlugin;
-use Spryker\Zed\CmsBlockStorage\Communication\Plugin\Event\CmsBlockEventResourcePlugin;
-use Spryker\Zed\CmsPageSearch\Communication\Plugin\Event\CmsPageEventResourcePlugin;
-use Spryker\Zed\CmsStorage\Communication\Plugin\Event\CmsEventResourcePlugin;
+use Spryker\Zed\AvailabilityStorage\Communication\Plugin\Event\AvailabilityEventResourceQueryContainerPlugin;
+use Spryker\Zed\CategoryPageSearch\Communication\Plugin\Event\CategoryPageEventResourceQueryContainerPlugin;
+use Spryker\Zed\CategoryStorage\Communication\Plugin\Event\CategoryNodeEventResourceQueryContainerPlugin;
+use Spryker\Zed\CategoryStorage\Communication\Plugin\Event\CategoryTreeEventResourceQueryContainerPlugin;
+use Spryker\Zed\CmsBlockCategoryStorage\Communication\Plugin\Event\CmsBlockCategoryEventResourceQueryContainerPlugin;
+use Spryker\Zed\CmsBlockProductStorage\Communication\Plugin\Event\CmsBlockProductEventResourceQueryContainerPlugin;
+use Spryker\Zed\CmsBlockStorage\Communication\Plugin\Event\CmsBlockEventResourceQueryContainerPlugin;
+use Spryker\Zed\CmsPageSearch\Communication\Plugin\Event\CmsPageEventResourceQueryContainerPlugin;
+use Spryker\Zed\CmsStorage\Communication\Plugin\Event\CmsEventResourceQueryContainerPlugin;
 use Spryker\Zed\EventBehavior\EventBehaviorDependencyProvider as SprykerEventBehaviorDependencyProvider;
-use Spryker\Zed\GlossaryStorage\Communication\Plugin\Event\GlossaryEventResourcePlugin;
-use Spryker\Zed\NavigationStorage\Communication\Plugin\Event\NavigationEventResourcePlugin;
-use Spryker\Zed\PriceProductStorage\Communication\Plugin\Event\PriceProductAbstractEventResourcePlugin;
-use Spryker\Zed\PriceProductStorage\Communication\Plugin\Event\PriceProductConcreteEventResourcePlugin;
-use Spryker\Zed\ProductCategoryFilterStorage\Communication\Plugin\Event\ProductCategoryFilterEventResourcePlugin;
-use Spryker\Zed\ProductCategoryStorage\Communication\Plugin\Event\ProductCategoryEventResourcePlugin;
-use Spryker\Zed\ProductGroupStorage\Communication\Plugin\Event\ProductGroupEventResourcePlugin;
-use Spryker\Zed\ProductImageStorage\Communication\Plugin\Event\ProductAbstractImageEventResourcePlugin;
-use Spryker\Zed\ProductImageStorage\Communication\Plugin\Event\ProductConcreteImageEventResourcePlugin;
-use Spryker\Zed\ProductLabelStorage\Communication\Plugin\Event\ProductAbstractLabelEventResourcePlugin;
-use Spryker\Zed\ProductLabelStorage\Communication\Plugin\Event\ProductLabelDictionaryEventResourcePlugin;
-use Spryker\Zed\ProductMeasurementUnitStorage\Communication\Plugin\Event\ProductConcreteMeasurementUnitEventResourcePlugin;
-use Spryker\Zed\ProductMeasurementUnitStorage\Communication\Plugin\Event\ProductMeasurementUnitEventResourcePlugin;
-use Spryker\Zed\ProductOptionStorage\Communication\Plugin\Event\ProductOptionEventResourcePlugin;
-use Spryker\Zed\ProductPageSearch\Communication\Plugin\Event\ProductPageEventResourcePlugin;
-use Spryker\Zed\ProductQuantityStorage\Communication\Plugin\Event\ProductQuantityEventResourcePlugin;
-use Spryker\Zed\ProductRelationStorage\Communication\Plugin\Event\ProductRelationEventResourcePlugin;
-use Spryker\Zed\ProductReviewSearch\Communication\Plugin\Event\ProductReviewEventResourcePlugin as ProductReviewSearchEventResourcePlugin;
-use Spryker\Zed\ProductReviewStorage\Communication\Plugin\Event\ProductReviewEventResourcePlugin;
-use Spryker\Zed\ProductSearchConfigStorage\Communication\Plugin\Event\ProductSearchConfigEventResourcePlugin;
-use Spryker\Zed\ProductSetPageSearch\Communication\Plugin\Event\ProductSetEventResourcePlugin as ProductSetPageSearchEventResourcePlugin;
-use Spryker\Zed\ProductSetStorage\Communication\Plugin\Event\ProductSetEventResourcePlugin;
-use Spryker\Zed\ProductStorage\Communication\Plugin\Event\ProductAbstractEventResourcePlugin;
-use Spryker\Zed\ProductStorage\Communication\Plugin\Event\ProductConcreteEventResourcePlugin;
-use Spryker\Zed\UrlStorage\Communication\Plugin\Event\RedirectEventResourcePlugin;
-use Spryker\Zed\UrlStorage\Communication\Plugin\Event\UrlEventResourcePlugin;
+use Spryker\Zed\GlossaryStorage\Communication\Plugin\Event\GlossaryEventResourceQueryContainerPlugin;
+use Spryker\Zed\NavigationStorage\Communication\Plugin\Event\NavigationEventResourceQueryContainerPlugin;
+use Spryker\Zed\PriceProductStorage\Communication\Plugin\Event\PriceProductAbstractEventResourceQueryContainerPlugin;
+use Spryker\Zed\PriceProductStorage\Communication\Plugin\Event\PriceProductConcreteEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductCategoryFilterStorage\Communication\Plugin\Event\ProductCategoryFilterEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductCategoryStorage\Communication\Plugin\Event\ProductCategoryEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductGroupStorage\Communication\Plugin\Event\ProductGroupEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductImageStorage\Communication\Plugin\Event\ProductAbstractImageEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductImageStorage\Communication\Plugin\Event\ProductConcreteImageEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductLabelStorage\Communication\Plugin\Event\ProductAbstractLabelEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductLabelStorage\Communication\Plugin\Event\ProductLabelDictionaryEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductMeasurementUnitStorage\Communication\Plugin\Event\ProductConcreteMeasurementUnitEventResourceRepositoryPlugin;
+use Spryker\Zed\ProductMeasurementUnitStorage\Communication\Plugin\Event\ProductMeasurementUnitEventResourceRepositoryPlugin;
+use Spryker\Zed\ProductOptionStorage\Communication\Plugin\Event\ProductOptionEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductPageSearch\Communication\Plugin\Event\ProductPageEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductQuantityStorage\Communication\Plugin\Event\ProductQuantityEventResourceRepositoryPlugin;
+use Spryker\Zed\ProductRelationStorage\Communication\Plugin\Event\ProductRelationEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductReviewSearch\Communication\Plugin\Event\ProductReviewEventResourceQueryContainerPlugin as ProductReviewSearchEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductReviewStorage\Communication\Plugin\Event\ProductReviewEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductSearchConfigStorage\Communication\Plugin\Event\ProductSearchConfigEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductSetPageSearch\Communication\Plugin\Event\ProductSetEventResourceQueryContainerPlugin as ProductSetPageSearchEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductSetStorage\Communication\Plugin\Event\ProductSetEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductStorage\Communication\Plugin\Event\ProductAbstractEventResourceQueryContainerPlugin;
+use Spryker\Zed\ProductStorage\Communication\Plugin\Event\ProductConcreteEventResourceQueryContainerPlugin;
+use Spryker\Zed\UrlStorage\Communication\Plugin\Event\RedirectEventResourceQueryContainerPlugin;
+use Spryker\Zed\UrlStorage\Communication\Plugin\Event\UrlEventResourceQueryContainerPlugin;
 
 class EventBehaviorDependencyProvider extends SprykerEventBehaviorDependencyProvider
 {
     /**
-     * @return \Spryker\Zed\EventBehavior\Dependency\Plugin\EventResourcePluginInterface[]
+     * @return \Spryker\Zed\EventBehavior\Dependency\Plugin\EventResourceQueryContainerPluginInterface[]
      */
     protected function getEventTriggerResourcePlugins()
     {
         return [
-            new CmsPageEventResourcePlugin(),
-            new CategoryPageEventResourcePlugin,
-            new ProductPageEventResourcePlugin(),
-            new ProductSetPageSearchEventResourcePlugin(),
-            new ProductReviewSearchEventResourcePlugin(),
-            new AvailabilityEventResourcePlugin(),
-            new CategoryTreeEventResourcePlugin(),
-            new CategoryNodeEventResourcePlugin(),
-            new CmsBlockCategoryEventResourcePlugin(),
-            new CmsBlockProductEventResourcePlugin(),
-            new CmsBlockEventResourcePlugin(),
-            new CmsEventResourcePlugin(),
-            new GlossaryEventResourcePlugin(),
-            new NavigationEventResourcePlugin(),
-            new PriceProductConcreteEventResourcePlugin(),
-            new PriceProductAbstractEventResourcePlugin(),
-            new ProductCategoryFilterEventResourcePlugin(),
-            new ProductCategoryEventResourcePlugin(),
-            new ProductGroupEventResourcePlugin(),
-            new ProductAbstractImageEventResourcePlugin(),
-            new ProductConcreteImageEventResourcePlugin(),
-            new ProductLabelDictionaryEventResourcePlugin(),
-            new ProductAbstractLabelEventResourcePlugin(),
-            new ProductOptionEventResourcePlugin(),
-            new ProductRelationEventResourcePlugin(),
-            new ProductReviewEventResourcePlugin(),
-            new ProductSearchConfigEventResourcePlugin(),
-            new ProductSetEventResourcePlugin(),
-            new ProductAbstractEventResourcePlugin(),
-            new ProductConcreteEventResourcePlugin(),
-            new UrlEventResourcePlugin(),
-            new RedirectEventResourcePlugin(),
-            new ProductMeasurementUnitEventResourcePlugin(),
-            new ProductConcreteMeasurementUnitEventResourcePlugin(),
-            new ProductQuantityEventResourcePlugin(),
+            new CmsPageEventResourceQueryContainerPlugin(),
+            new CategoryPageEventResourceQueryContainerPlugin,
+            new ProductPageEventResourceQueryContainerPlugin(),
+            new ProductSetPageSearchEventResourceQueryContainerPlugin(),
+            new ProductReviewSearchEventResourceQueryContainerPlugin(),
+            new AvailabilityEventResourceQueryContainerPlugin(),
+            new CategoryTreeEventResourceQueryContainerPlugin(),
+            new CategoryNodeEventResourceQueryContainerPlugin(),
+            new CmsBlockCategoryEventResourceQueryContainerPlugin(),
+            new CmsBlockProductEventResourceQueryContainerPlugin(),
+            new CmsBlockEventResourceQueryContainerPlugin(),
+            new CmsEventResourceQueryContainerPlugin(),
+            new GlossaryEventResourceQueryContainerPlugin(),
+            new NavigationEventResourceQueryContainerPlugin(),
+            new PriceProductConcreteEventResourceQueryContainerPlugin(),
+            new PriceProductAbstractEventResourceQueryContainerPlugin(),
+            new ProductCategoryFilterEventResourceQueryContainerPlugin(),
+            new ProductCategoryEventResourceQueryContainerPlugin(),
+            new ProductGroupEventResourceQueryContainerPlugin(),
+            new ProductAbstractImageEventResourceQueryContainerPlugin(),
+            new ProductConcreteImageEventResourceQueryContainerPlugin(),
+            new ProductLabelDictionaryEventResourceQueryContainerPlugin(),
+            new ProductAbstractLabelEventResourceQueryContainerPlugin(),
+            new ProductOptionEventResourceQueryContainerPlugin(),
+            new ProductRelationEventResourceQueryContainerPlugin(),
+            new ProductReviewEventResourceQueryContainerPlugin(),
+            new ProductSearchConfigEventResourceQueryContainerPlugin(),
+            new ProductSetEventResourceQueryContainerPlugin(),
+            new ProductAbstractEventResourceQueryContainerPlugin(),
+            new ProductConcreteEventResourceQueryContainerPlugin(),
+            new UrlEventResourceQueryContainerPlugin(),
+            new RedirectEventResourceQueryContainerPlugin(),
+            new ProductMeasurementUnitEventResourceRepositoryPlugin(),
+            new ProductConcreteMeasurementUnitEventResourceRepositoryPlugin(),
+            new ProductQuantityEventResourceRepositoryPlugin(),
         ];
     }
 }
