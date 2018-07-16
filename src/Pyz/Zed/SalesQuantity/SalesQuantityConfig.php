17461dd000c15570a6ca136e1a5261ad90894067
Karoly Gerner <karoly.gerner@spryker.com>
2018-07-16 17:32:22 +0000

    PS-955 Adjusting comment

--- src/Pyz/Zed/SalesQuantity/SalesQuantityConfig.php
+++ src/Pyz/Zed/SalesQuantity/SalesQuantityConfig.php
@@ -11,5 +11,8 @@ use Spryker\Zed\SalesQuantity\SalesQuantityConfig as SprykerSalesQuantityConfig;
 
 class SalesQuantityConfig extends SprykerSalesQuantityConfig
 {
+    /**
+     * @see \Spryker\Zed\SalesQuantity\SalesQuantityConfig::ITEM_NONSPLIT_QUANTITY_THRESHOLD
+     */
     protected const ITEM_NONSPLIT_QUANTITY_THRESHOLD = 10;
 }
