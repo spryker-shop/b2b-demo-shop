209a265a9cbf592e5a67dc6077b53404629776b6
zamlelyi <nikolay.zamlely@spryker.com>
2018-08-22 20:09:38 +0300

    b2bs-51: fixes after CR

--- src/Pyz/Yves/ProductReviewWidget/Theme/default/components/molecules/rating-selector/index.ts
+++ src/Pyz/Yves/ProductReviewWidget/Theme/default/components/molecules/rating-selector/index.ts
@@ -1,3 +1,1 @@
-import './rating-selector.scss';
-import register from 'ShopUi/app/registry';
-export default register('rating-selector', () => import(/* webpackMode: "lazy" */'./rating-selector'));
+import './rating-selector';
