0aebbd20654cd42af011238a4d1bcb38670c628b
maslov <dev.maslov.a@gmail.com>
2018-08-22 13:08:29 +0300

    Fix pull request

--- src/Pyz/Yves/ProductReviewWidget/Theme/default/components/molecules/rating-selector/index.ts
+++
@@ -1,3 +0,0 @@
-import './rating-selector.scss';
-import register from 'ShopUi/app/registry';
-export default register('rating-selector', () => import(/* webpackMode: "lazy" */'./rating-selector'));
