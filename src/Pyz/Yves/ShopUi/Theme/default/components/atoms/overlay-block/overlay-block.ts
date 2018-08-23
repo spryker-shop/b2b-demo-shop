e83d33f6e05e831d52a98a26bfbc104d4b50be26
maslov <dev.maslov.a@gmail.com>
2018-08-23 17:43:43 +0300

    fixed overlay

--- src/Pyz/Yves/ShopUi/Theme/default/components/atoms/overlay-block/overlay-block.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/atoms/overlay-block/overlay-block.ts
@@ -10,10 +10,22 @@ export default class OverlayBlock extends Component {
         }
     }
 
-    toggleOverlay(modifire?: string, bodyModifire?: string): void {
-        this.toggleModifire(modifire, this, this.name);
-        this.toggleModifire(bodyModifire, document.body,'body-overlay');
-        this.classList.toggle(this.classToShow);
+    protected toggleCondition(isOpen): boolean {
+        if (isOpen !== undefined) {
+            const isShown = this.classList.contains(this.classToShow);
+            return (isOpen && !isShown) || (!isOpen && isShown);
+        }
+        return true;
+    }
+
+    toggleOverlay(modifire?: string, bodyModifire?: string, isOpen?: boolean): void {
+
+        if(this.toggleCondition(isOpen)) {
+            this.toggleModifire(modifire, this, this.name);
+            this.toggleModifire(bodyModifire, document.body,'body-overlay');
+            this.classList.toggle(this.classToShow);
+        }
+
     }
 
     get classToShow(): string {
--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/suggest-search/suggest-search.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/suggest-search/suggest-search.ts
@@ -220,14 +220,14 @@ export default class SuggestSearch extends Component {
         this.suggestionsContainer.classList.remove('is-hidden');
         this.searchInput.classList.add(`${this.name}__input--active`);
         this.hintInput.classList.add(`${this.name}__hint--active`);
-        this.overlay.toggleOverlay('no-search', 'no-search');
+        this.overlay.toggleOverlay('no-search', 'no-search', true);
     }
 
     hideSugestions(): void {
         this.suggestionsContainer.classList.add('is-hidden');
         this.searchInput.classList.remove(`${this.name}__input--active`);
         this.hintInput.classList.remove(`${this.name}__hint--active`);
-        this.overlay.toggleOverlay('no-search', 'no-search');
+        this.overlay.toggleOverlay('no-search', 'no-search', false);
     }
 
     protected createHintInput(): void {
