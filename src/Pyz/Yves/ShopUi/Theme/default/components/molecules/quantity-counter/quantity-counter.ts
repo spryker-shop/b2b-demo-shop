efa9ba1b2283199c10a563c7c852e68d5925ad6d
Yuriy Gerton <yuriy.gerton@spryker.com>
2018-08-29 19:10:43 +0300

    B2BS-111: Add autoupdate for quantity.

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/quantity-counter/quantity-counter.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/quantity-counter/quantity-counter.ts
@@ -9,6 +9,10 @@ export default class QuantityCounter extends Component {
         let maxQuantity = input.data('max-quantity');
         const decrButton = $(this).find('.js-quantity-decrement');
         const incrButton = $(this).find('.js-quantity-increment');
+        const autoUpdate = input.data('auto-update');
+        const form = $(this).parent('form');
+        let timeout = 0;
+
         if(!maxQuantity){
             maxQuantity = Infinity;
         }
@@ -16,14 +20,30 @@ export default class QuantityCounter extends Component {
             let value = +input.val();
             if(value > 1){
                 input.val(value - 1);
+
+                if(autoUpdate) {
+                    timer();
+                }
             }
         });
         incrButton.click(function () {
             let value = +input.val();
             if(value < maxQuantity) {
                 input.val(value + 1);
+
+                if(autoUpdate) {
+                    timer();
+                }
             }
         });
 
+        if(autoUpdate) {
+            input.change(timer);
+        }
+
+        function timer() {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => form.submit(), 1000);
+        }
     }
 }
