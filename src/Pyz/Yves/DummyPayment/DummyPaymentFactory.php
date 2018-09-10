b6ff64bb034f774cc89910c1776a0e6dd76d2504
Alexander Veselov <alexander.veselov@spryker.com>
2018-09-10 10:52:58 +0000

    B2BS-370: remove birthday field

--- src/Pyz/Yves/DummyPayment/DummyPaymentFactory.php
+++ src/Pyz/Yves/DummyPayment/DummyPaymentFactory.php
@@ -5,54 +5,18 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Yves\DummyPayment;
+namespace Pyz\Yves\DummyPayment;
 
-use Spryker\Yves\DummyPayment\Form\CreditCardSubForm;
-use Spryker\Yves\DummyPayment\Form\DataProvider\DummyPaymentCreditCardFormDataProvider;
-use Spryker\Yves\DummyPayment\Form\DataProvider\DummyPaymentInvoiceFormDataProvider;
-use Spryker\Yves\DummyPayment\Form\InvoiceSubForm;
-use Spryker\Yves\DummyPayment\Handler\DummyPaymentHandler;
-use Spryker\Yves\Kernel\AbstractFactory;
+use Pyz\Yves\DummyPayment\Form\InvoiceSubForm;
+use Spryker\Yves\DummyPayment\DummyPaymentFactory as SprykerDummyPaymentFactory;
 
-class DummyPaymentFactory extends AbstractFactory
+class DummyPaymentFactory extends SprykerDummyPaymentFactory
 {
-    /**
-     * @return \Spryker\Yves\DummyPayment\Form\CreditCardSubForm
-     */
-    public function createCreditCardForm()
-    {
-        return new CreditCardSubForm();
-    }
-
-    /**
-     * @return \Spryker\Yves\DummyPayment\Form\DataProvider\DummyPaymentCreditCardFormDataProvider
-     */
-    public function createCreditCardFormDataProvider()
-    {
-        return new DummyPaymentCreditCardFormDataProvider();
-    }
-
     /**
      * @return \Spryker\Yves\DummyPayment\Form\InvoiceSubForm
      */
     public function createInvoiceForm()
     {
         return new InvoiceSubForm();
-    }
-
-    /**
-     * @return \Spryker\Yves\DummyPayment\Form\DataProvider\DummyPaymentInvoiceFormDataProvider
-     */
-    public function createInvoiceFormDataProvider()
-    {
-        return new DummyPaymentInvoiceFormDataProvider();
-    }
-
-    /**
-     * @return \Spryker\Yves\DummyPayment\Handler\DummyPaymentHandler
-     */
-    public function createDummyPaymentHandler()
-    {
-        return new DummyPaymentHandler();
     }
 }
--- src/Pyz/Yves/DummyPayment/Form/InvoiceSubForm.php
+++ src/Pyz/Yves/DummyPayment/Form/InvoiceSubForm.php
@@ -5,66 +5,13 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Yves\DummyPayment\Form;
+namespace Pyz\Yves\DummyPayment\Form;
 
-use Generated\Shared\Transfer\DummyPaymentTransfer;
-use Spryker\Shared\DummyPayment\DummyPaymentConfig;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Spryker\Yves\DummyPayment\Form\InvoiceSubForm as SprykerInvoiceSubForm;
 
-class InvoiceSubForm extends AbstractSubForm
+class InvoiceSubForm extends SprykerInvoiceSubForm
 {
-    const PAYMENT_METHOD = 'invoice';
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        return DummyPaymentConfig::PAYMENT_METHOD_INVOICE;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPropertyPath()
-    {
-        return DummyPaymentConfig::PAYMENT_METHOD_INVOICE;
-    }
-
-    /**
-     * @return string
-     */
-    public function getTemplatePath()
-    {
-        return DummyPaymentConfig::PROVIDER_NAME . '/' . self::PAYMENT_METHOD;
-    }
-
-    /**
-     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
-     *
-     * @return void
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => DummyPaymentTransfer::class,
-        ])->setRequired(self::OPTIONS_FIELD_NAME);
-    }
-
-    /**
-     * @deprecated Use `configureOptions()` instead.
-     *
-     * @param \Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver
-     *
-     * @return void
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $this->configureOptions($resolver);
-    }
-
     /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      * @param array $options
@@ -73,6 +20,6 @@ class InvoiceSubForm extends AbstractSubForm
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-       // $this->addDateOfBirth($builder);
+
     }
 }
