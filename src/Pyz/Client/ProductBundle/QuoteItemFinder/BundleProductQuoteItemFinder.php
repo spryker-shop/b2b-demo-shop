b8ce7f64cdf358ed4b540c0291c31c1f3c857010
theodoros.liokos@spryker.com <theodoros.liokos@spryker.com>
2018-09-10 11:02:05 +0200

    B2BS-375: Fixed exception when user tries to add bundle item into cart twice

---
+++ src/Pyz/Client/ProductBundle/QuoteItemFinder/BundleProductQuoteItemFinder.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Client\ProductBundle\QuoteItemFinder;
+
+use Generated\Shared\Transfer\QuoteTransfer;
+use Spryker\Client\ProductBundle\QuoteItemFinder\BundleProductQuoteItemFinder as SprykerBundleProductQuoteItemFinder;
+
+class BundleProductQuoteItemFinder extends SprykerBundleProductQuoteItemFinder
+{
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     * @param string $groupKey
+     *
+     * @return int
+     */
+    protected function getBundledProductTotalQuantity(QuoteTransfer $quoteTransfer, ?string $groupKey = null): int
+    {
+        $bundleItemQuantity = 0;
+        foreach ($quoteTransfer->getBundleItems() as $bundleItemTransfer) {
+            if ($bundleItemTransfer->getGroupKey() !== $groupKey) {
+                continue;
+            }
+            $bundleItemQuantity += $bundleItemTransfer->getQuantity();
+        }
+
+        return $bundleItemQuantity;
+    }
+}
