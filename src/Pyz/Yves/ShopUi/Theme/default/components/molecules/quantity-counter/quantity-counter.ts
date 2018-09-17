bec28c2e67ec5e30aca7f32c3ec742958dbafc0d
maslov <dev.maslov.a@gmail.com>
2018-09-17 14:36:40 +0300

    refactored quantity counter

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/quantity-counter/quantity-counter.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/quantity-counter/quantity-counter.ts
@@ -1,56 +1,78 @@
 import Component from 'ShopUi/models/component';
-import $ from 'jquery/dist/jquery';
 
 export default class QuantityCounter extends Component {
 
-    timeout = null;
+    readonly incrButton: HTMLButtonElement
+    readonly decrButton: HTMLButtonElement
+    readonly input: HTMLInputElement
+
+    timeout: number
+
+    constructor() {
+        super();
+        this.incrButton = <HTMLButtonElement>this.querySelector(`.${this.jsName}__button-increment`);
+        this.decrButton = <HTMLButtonElement>this.querySelector(`.${this.jsName}__button-decrement`);
+        this.input = <HTMLInputElement>this.querySelector(`.${this.jsName}__input`);
+        this.timeout = 0;
+    }
 
     protected readyCallback(): void {
+        this.mapEvents();
+    }
 
-        const input = $(this).find(`.${this.name}__input`);
-        let maxQuantity = input.data('max-quantity');
-        const decrButton = $(this).find('.js-quantity-decrement');
-        const incrButton = $(this).find('.js-quantity-increment');
-        const autoUpdate = input.data('auto-update');
-        const form = $(this).parent('form');
-
-        const self = this;
-
-        if(!maxQuantity){
-            maxQuantity = Infinity;
-        }
-        decrButton.click(() => {
-            let value = +input.val();
-            if(value > 1){
-                input.val(value - 1);
-
-                const event = new CustomEvent('quantityChange');
-                self.querySelector(`.${self.name}__input`).dispatchEvent(event);
-                if(autoUpdate) {
-                    this.timer(form);
-                }
-            }
-        });
-        incrButton.click(() => {
-            let value = +input.val();
-            if(value < maxQuantity) {
-                input.val(value + 1);
-
-                const event = new CustomEvent('quantityChange');
-                self.querySelector(`.${self.name}__input`).dispatchEvent(event);
-                if(autoUpdate) {
-                    this.timer(form);
-                }
-            }
-        });
-
-        if(autoUpdate) {
-            input.change(() => this.timer(form));
+    protected mapEvents(): void {
+        this.incrButton.addEventListener('click', (event: Event) => this.incrementValue(event));
+        this.decrButton.addEventListener('click', (event: Event) => this.decrementValue(event));
+        if(this.autoUpdate) {
+            this.input.addEventListener('change', () => this.timer());
         }
     }
 
-    protected timer(form): void {
+    protected incrementValue(event): void {
+        event.preventDefault();
+        const VALUE = +this.input.value;
+
+        if(VALUE <= this.maxQuantity) {
+            this.input.stepUp();
+            this.triggerInputEvent();
+        }
+    }
+
+    protected decrementValue(event): void {
+        event.preventDefault();
+        const VALUE = +this.input.value;
+
+        if(VALUE - this.step >= this.minQuantity) {
+            this.input.stepDown();
+            this.triggerInputEvent();
+        }
+    }
+
+    protected triggerInputEvent(): void {
+        const CHANGE_EVENT = new Event('change');
+        this.input.dispatchEvent(CHANGE_EVENT);
+    }
+
+    protected timer(): void {
         clearTimeout(this.timeout);
-        this.timeout = setTimeout(() => form.submit(), 1000);
+        this.timeout = setTimeout(() => this.input.form.submit(), 1000);
+    }
+
+    get minQuantity(): number {
+        return +this.input.getAttribute('min');
+    }
+
+    get maxQuantity(): number {
+        const MAX = +this.input.getAttribute('max');
+        return MAX > 0 && MAX > this.minQuantity ? MAX : Infinity;
+    }
+
+    get step(): number {
+        const STEP = +this.input.getAttribute('step');
+        return STEP > 0 ? STEP : 1;
+    }
+
+    get autoUpdate(): boolean {
+        return !!this.input.dataset.autoUpdate;
     }
 }
