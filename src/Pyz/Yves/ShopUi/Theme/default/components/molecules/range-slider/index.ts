21820d4880c9178439abeae6a112e4d7d97bebe0
Yuriy Gerton <yuriy.gerton@spryker.com>
2018-09-19 13:53:50 +0300

    B2BS-480: Catalog page refactored.

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/range-slider/index.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/range-slider/index.ts
@@ -1,4 +1,4 @@
 import register from 'ShopUi/app/registry';
 export default register('range-slider', () => import(/* webpackMode: "lazy" */'./range-slider'));
-import './nouislider.min.scss';
+import 'nouislider/distribute/nouislider.css';
 import './range-slider.scss';
