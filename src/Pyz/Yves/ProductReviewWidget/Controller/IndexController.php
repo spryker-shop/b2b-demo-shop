8b218eb7e596a6a3abe7bcae912e61d4d73498df
Eugene Grebenikov <eugene.grebenikov@spryker.com>
2018-09-27 08:01:24 +0000

    B2BS-487: Add review form

---
+++ src/Pyz/Yves/ProductReviewWidget/Controller/IndexController.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace SprykerShop\Yves\ProductReviewWidget\Controller;
+
+use Generated\Shared\Transfer\ProductReviewSearchRequestTransfer;
+use Spryker\Shared\Storage\StorageConstants;
+use SprykerShop\Yves\ShopApplication\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @method \SprykerShop\Yves\ProductReviewWidget\ProductReviewWidgetFactory getFactory()
+ */
+class IndexController extends AbstractController
+{
+    const STORAGE_CACHE_STRATEGY = StorageConstants::STORAGE_CACHE_STRATEGY_INACTIVE;
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Spryker\Yves\Kernel\View\View
+     */
+    public function indexAction(Request $request)
+    {
+        $viewData = $this->executeIndexAction($request);
+
+        return $this->view($viewData, [], '@ProductReviewWidget/views/review-overview/review-overview.twig');
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return array
+     */
+    protected function executeIndexAction(Request $request): array
+    {
+        $parentRequest = $this->getParentRequest();
+        $idProductAbstract = $request->attributes->get('idProductAbstract');
+
+        $customer = $this->getFactory()->getCustomerClient()->getCustomer();
+        $hasCustomer = $customer !== null;
+
+        $productReviewSearchRequestTransfer = new ProductReviewSearchRequestTransfer();
+        $productReviewSearchRequestTransfer->setIdProductAbstract($idProductAbstract);
+        $productReviewSearchRequestTransfer->setRequestParams($parentRequest->query->all());
+        $productReviews = $this->getFactory()
+            ->getProductReviewClient()
+            ->findProductReviewsInSearch($productReviewSearchRequestTransfer);
+
+        return [
+            'hasCustomer' => $hasCustomer,
+            'productReviews' => $productReviews['productReviews'],
+            'pagination' => $productReviews['pagination'],
+            'summary' => $this->getFactory()->createProductReviewSummaryCalculator()->execute($productReviews['ratingAggregation']),
+            'maximumRating' => $this->getFactory()->getProductReviewClient()->getMaximumRating(),
+            'idProductAbstract' => $idProductAbstract,
+        ];
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Request
+     */
+    protected function getParentRequest()
+    {
+        return $this->getApplication()['request_stack']->getParentRequest();
+    }
+}
