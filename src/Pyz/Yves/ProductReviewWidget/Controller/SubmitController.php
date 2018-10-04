c9c208594e8b0976ef3a6eb7b83f70c2b9dddcac
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2018-10-04 12:45:31 +0000

    B2B-588: fix review form

---
+++ src/Pyz/Yves/ProductReviewWidget/Controller/SubmitController.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Yves\ProductReviewWidget\Controller;
+
+use SprykerShop\Yves\ProductReviewWidget\Controller\SubmitController as SprykerSubmitController;
+use Symfony\Component\HttpFoundation\Request;
+
+class SubmitController extends SprykerSubmitController
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Request
+     */
+    protected function getParentRequest(): Request
+    {
+        return $this->getApplication()['request_stack']->getMasterRequest();
+    }
+}
