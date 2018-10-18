ca3cb0cb341b91a7da21960109e8c765e7878703
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2018-10-18 12:46:23 +0300

    B2BS-526: Integrate Attach customer to Company BU in Zed

---
+++ src/Pyz/Zed/CompanyUserGui/CompanyUserGuiDependencyProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\CompanyUserGui;
+
+use Spryker\Zed\CompanyBusinessUnitGui\Communication\Plugin\CompanyUserGui\CompanyBusinessUnitAttachCustomerFormExpanderPlugin;
+use Spryker\Zed\CompanyBusinessUnitGui\Communication\Plugin\CompanyUserGui\CompanyBusinessUnitCompanyUserTableConfigExpanderPlugin;
+use Spryker\Zed\CompanyBusinessUnitGui\Communication\Plugin\CompanyUserGui\CompanyBusinessUnitCompanyUserTablePrepareDataExpanderPlugin;
+use Spryker\Zed\CompanyBusinessUnitGui\Communication\Plugin\CompanyUserGui\CompanyBusinessUnitFormExpanderPlugin;
+use Spryker\Zed\CompanyRoleGui\Communication\Plugin\CompanyUserGui\CompanyRoleAttachCustomerFormExpanderPlugin;
+use Spryker\Zed\CompanyRoleGui\Communication\Plugin\CompanyUserGui\CompanyRoleCompanyUserTableConfigExpanderPlugin;
+use Spryker\Zed\CompanyRoleGui\Communication\Plugin\CompanyUserGui\CompanyRoleCompanyUserTablePrepareDataExpanderPlugin;
+use Spryker\Zed\CompanyRoleGui\Communication\Plugin\CompanyUserGui\CompanyRoleFormExpanderPlugin;
+use Spryker\Zed\CompanyUserGui\CompanyUserGuiDependencyProvider as SprykerCompanyUserGuiDependencyProvider;
+
+class CompanyUserGuiDependencyProvider extends SprykerCompanyUserGuiDependencyProvider
+{
+    /**
+     * @return \Spryker\Zed\CompanyUserGuiExtension\Dependency\Plugin\CompanyUserTableConfigExpanderPluginInterface[]
+     */
+    protected function getCompanyUserTableConfigExpanderPlugins(): array
+    {
+        return [
+            new CompanyRoleCompanyUserTableConfigExpanderPlugin(),
+            new CompanyBusinessUnitCompanyUserTableConfigExpanderPlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Zed\CompanyUserGuiExtension\Dependency\Plugin\CompanyUserTablePrepareDataExpanderPluginInterface[]
+     */
+    protected function getCompanyUserTablePrepareDataExpanderPlugins(): array
+    {
+        return [
+            new CompanyRoleCompanyUserTablePrepareDataExpanderPlugin(),
+            new CompanyBusinessUnitCompanyUserTablePrepareDataExpanderPlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Zed\CompanyUserGuiExtension\Dependency\Plugin\CompanyUserFormExpanderPluginInterface[]
+     */
+    protected function getCompanyUserFormExpanderPlugins(): array
+    {
+        return [
+            new CompanyBusinessUnitFormExpanderPlugin(),
+            new CompanyRoleFormExpanderPlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Zed\CompanyUserGuiExtension\Dependency\Plugin\CompanyUserAttachCustomerFormExpanderPluginInterface[]
+     */
+    protected function getCompanyUserAttachCustomerFormExpanderPlugins(): array
+    {
+        return [
+            new CompanyBusinessUnitAttachCustomerFormExpanderPlugin(),
+            new CompanyRoleAttachCustomerFormExpanderPlugin(),
+        ];
+    }
+}
