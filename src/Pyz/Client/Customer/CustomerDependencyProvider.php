d260fd83d857b32a3f0e020ef279ad3c0f5d384b
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2018-10-21 21:10:52 +0000

    B2BS-665: fix missing carts after dismiss

--- src/Pyz/Client/Customer/CustomerDependencyProvider.php
+++ src/Pyz/Client/Customer/CustomerDependencyProvider.php
@@ -33,9 +33,9 @@ class CustomerDependencyProvider extends SprykerCustomerDependencyProvider
     protected function getCustomerSessionSetPlugins()
     {
         return [
+            new CustomerChangeCartUpdatePlugin(),
             new GuestCartSaveCustomerSessionSetPlugin(), #MultiCartFeature
             new GuestCartUpdateCustomerSessionSetPlugin(), #PersistentCartFeature
-            new CustomerChangeCartUpdatePlugin(),
             new CustomerChangePriceUpdatePlugin(), #PricesPerBusinessUnit
         ];
     }
