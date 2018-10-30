46d007a6a9aa6c1d42534106e8a7afc4782014e0
Alexander Veselov <alexander.veselov@spryker.com>
2018-10-30 14:45:42 +0000

    B2BS-727: check permission for see and add for cart

---
+++ src/Pyz/Yves/QuickOrderPage/Controller/QuickOrderController.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Yves\QuickOrderPage\Controller;
+
+use Spryker\Yves\Kernel\PermissionAwareTrait;
+use SprykerShop\Shared\CartPage\Plugin\AddCartItemPermissionPlugin;
+use SprykerShop\Yves\CartPage\Plugin\Provider\CartControllerProvider;
+use SprykerShop\Yves\CheckoutPage\Plugin\Provider\CheckoutPageControllerProvider;
+use SprykerShop\Yves\QuickOrderPage\Form\QuickOrderForm;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use SprykerShop\Yves\QuickOrderPage\Controller\QuickOrderController as SprykerQuickOrderController;
+/**
+ * @method \SprykerShop\Yves\QuickOrderPage\QuickOrderPageFactory getFactory()
+ */
+class QuickOrderController extends SprykerQuickOrderController
+{
+    use PermissionAwareTrait;
+
+    /**
+     * @param \Symfony\Component\Form\FormInterface $quickOrderForm
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|null
+     */
+    protected function handleQuickOrderForm(FormInterface $quickOrderForm, Request $request): ?RedirectResponse
+    {
+        if ($quickOrderForm->isSubmitted() && $quickOrderForm->isValid()) {
+            $quickOrder = $quickOrderForm->getData();
+
+            if ($request->get(QuickOrderForm::SUBMIT_BUTTON_ADD_TO_CART) !== null) {
+                if (!$this->can(AddCartItemPermissionPlugin::KEY)) {
+                    $this->addErrorMessage("Access Denied");
+
+                    return null;
+                }
+                $result = $this->getFactory()
+                    ->createFormOperationHandler()
+                    ->addToCart($quickOrder);
+
+                if (!$result) {
+                    return null;
+                }
+
+                return $this->redirectResponseInternal(CartControllerProvider::ROUTE_CART);
+            }
+
+            if ($request->get(QuickOrderForm::SUBMIT_BUTTON_CREATE_ORDER) !== null) {
+                $result = $this->getFactory()
+                    ->createFormOperationHandler()
+                    ->createOrder($quickOrder);
+
+                if (!$result) {
+                    return null;
+                }
+
+                return $this->redirectResponseInternal(CheckoutPageControllerProvider::CHECKOUT_INDEX);
+            }
+        }
+
+        return null;
+    }
+}
