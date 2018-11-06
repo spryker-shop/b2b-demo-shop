2bf762dc251b6124331c872b4590da188d13e7be
Alexander Veselov <alexander.veselov@spryker.com>
2018-11-06 14:11:35 +0000

    B2BS-781: remove AddThresholdMessagesCartPreReloadItemsPlugin

--- src/Pyz/Zed/Cart/CartDependencyProvider.php
+++ src/Pyz/Zed/Cart/CartDependencyProvider.php
@@ -53,7 +53,6 @@ use Spryker\Zed\ProductPackagingUnit\Communication\Plugin\Cart\ProductPackagingU
 use Spryker\Zed\ProductQuantity\Communication\Plugin\Cart\ProductQuantityRestrictionCartPreCheckPlugin;
 use Spryker\Zed\ProductQuantity\Communication\Plugin\CartExtension\ProductQuantityRestrictionCartRemovalPreCheckPlugin;
 use Spryker\Zed\SalesOrderThreshold\Communication\Plugin\Cart\AddThresholdMessagesCartPostReloadItemsPlugin;
-use Spryker\Zed\SalesOrderThreshold\Communication\Plugin\Cart\AddThresholdMessagesCartPreReloadItemsPlugin;
 use Spryker\Zed\SalesQuantity\Communication\Plugin\Cart\IsQuantitySplittableItemExpanderPlugin;
 use Spryker\Zed\ShipmentCartConnector\Communication\Plugin\Cart\CartShipmentExpanderPlugin;
 use Spryker\Zed\ShipmentCartConnector\Communication\Plugin\Cart\CartShipmentPreCheckPlugin;
@@ -140,7 +139,6 @@ class CartDependencyProvider extends SprykerCartDependencyProvider
             new CartBundleItemsPreReloadPlugin(),
             new RemoveInactiveItemsPreReloadPlugin(),
             new RemoveRestrictedItemsPreReloadPlugin(),
-            new AddThresholdMessagesCartPreReloadItemsPlugin(), #SalesOrderThresholdFeature
             new FilterItemsWithoutPricePlugin(),
         ];
     }
