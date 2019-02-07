6cbc8203889fb39fcec89fdc9c16dfb4c19be7de
zamlelyi <nikolay.zamlely@spryker.com>
2019-02-07 18:30:42 +0200

    PS-7332: Fixes after CR

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/autocomplete-form/index.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/autocomplete-form/index.ts
@@ -1,4 +1,3 @@
-// import './autocomplete-form.scss';
-//
-// import register from 'ShopUi/app/registry';
-// export default register('autocomplete-form', () => import(/* webpackMode: "lazy" */'./autocomplete-form'));
+import './autocomplete-form.scss';
+import register from 'ShopUi/app/registry';
+export default register('autocomplete-form', () => import(/* webpackMode: "lazy" */'./autocomplete-form'));
