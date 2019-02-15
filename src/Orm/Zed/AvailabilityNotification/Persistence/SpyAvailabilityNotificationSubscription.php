fc8067efd6e10de0aea9c5ffe2095088e4b3c234
Aleksandr Kovalchuk <avtop25495@gmail.com>
2019-02-15 17:57:43 +0200

    CC-2355 fixe travis

---
+++ src/Orm/Zed/AvailabilityNotification/Persistence/SpyAvailabilityNotificationSubscription.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\AvailabilityNotification\Persistence;
+
+use Spryker\Zed\AvailabilityNotification\Persistence\Propel\AbstractSpyAvailabilityNotificationSubscription as BaseSpyAvailabilityNotificationSubscription;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_availability_notification_subscription' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyAvailabilityNotificationSubscription extends BaseSpyAvailabilityNotificationSubscription
+{
+
+}
---
+++ src/Orm/Zed/AvailabilityNotification/Persistence/SpyAvailabilityNotificationSubscriptionQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\AvailabilityNotification\Persistence;
+
+use Spryker\Zed\AvailabilityNotification\Persistence\Propel\AbstractSpyAvailabilityNotificationSubscriptionQuery as BaseSpyAvailabilityNotificationSubscriptionQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_availability_notification_subscription' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyAvailabilityNotificationSubscriptionQuery extends BaseSpyAvailabilityNotificationSubscriptionQuery
+{
+
+}
