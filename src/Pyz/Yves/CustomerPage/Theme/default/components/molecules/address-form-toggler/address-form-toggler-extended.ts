b21b90f49f3286400679f69213ba56c523d9aa1c
danil.moroz <danil.moroz@spryker.com>
2019-04-25 15:34:45 +0300

    PS-9170: IE11. Broken CSS

---
+++ src/Pyz/Yves/CustomerPage/Theme/default/components/molecules/address-form-toggler/address-form-toggler-extended.ts
@@ -0,0 +1,19 @@
+import AddressFormToggler from 'CustomerPage/components/molecules/address-form-toggler/address-form-toggler';
+
+export default class AddressFormTogglerExtended extends AddressFormToggler {
+    protected readyCallback(): void {
+        super.readyCallback();
+    }
+
+    toggle(isShown: boolean): void {
+        const hasCompanyBusinessUnitAddress = (this.hasCompanyBusinessUnitAddress == 'true');
+
+        if (hasCompanyBusinessUnitAddress || !isShown) {
+            this.form.classList.remove(this.classToToggle);
+
+            return;
+        }
+
+        this.form.classList.add(this.classToToggle);
+    }
+}
