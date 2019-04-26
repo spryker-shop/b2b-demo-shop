8d9d5f94a99a472e6f3bd546d3ff7d1218ef0edd
danil.moroz <danil.moroz@spryker.com>
2019-04-26 13:12:27 +0300

    PS-9170: Fix code reviews

--- src/Pyz/Yves/CustomerPage/Theme/default/components/molecules/address-form-toggler/address-form-toggler-extended.ts
+++ src/Pyz/Yves/CustomerPage/Theme/default/components/molecules/address-form-toggler/address-form-toggler-extended.ts
@@ -1,12 +1,8 @@
 import AddressFormToggler from 'CustomerPage/components/molecules/address-form-toggler/address-form-toggler';
 
 export default class AddressFormTogglerExtended extends AddressFormToggler {
-    protected readyCallback(): void {
-        super.readyCallback();
-    }
-
     toggle(isShown: boolean): void {
-        const hasCompanyBusinessUnitAddress = (this.hasCompanyBusinessUnitAddress === 'true');
+        const hasCompanyBusinessUnitAddress = <boolean>(this.hasCompanyBusinessUnitAddress === 'true');
 
         if (hasCompanyBusinessUnitAddress || !isShown) {
             this.form.classList.remove(this.classToToggle);
