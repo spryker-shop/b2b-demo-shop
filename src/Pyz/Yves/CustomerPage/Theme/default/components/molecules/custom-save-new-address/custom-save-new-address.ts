9c5e1c26e820388ef0f209d0389e6da0107770a6
zamlelyi <nikolay.zamlely@spryker.com>
2019-05-02 15:20:47 +0300

    PS-9241: Fixes for check-box to save address when own customer address is selected

---
+++ src/Pyz/Yves/CustomerPage/Theme/default/components/molecules/custom-save-new-address/custom-save-new-address.ts
@@ -0,0 +1,8 @@
+import SaveNewAddress from 'CustomerPage/components/molecules/save-new-address/save-new-address';
+
+export default class CustomSaveNewAddress extends SaveNewAddress {
+
+    protected readyCallback(): void {
+        super.readyCallback()
+    }
+}
---
+++ src/Pyz/Yves/CustomerPage/Theme/default/components/molecules/custom-save-new-address/index.ts
@@ -0,0 +1,2 @@
+import register from 'ShopUi/app/registry';
+export default register('custom-save-new-address', () => import(/* webpackMode: "lazy" */'./custom-save-new-address'));
