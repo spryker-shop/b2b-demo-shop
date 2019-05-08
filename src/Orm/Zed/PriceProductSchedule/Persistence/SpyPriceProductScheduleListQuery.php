99c099b7bf885c984f43cb4f754aecfbdc6c531e
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-05-08 16:56:21 +0300

    CC-4711. Integrated PriceProductSchedule module
    * Integrated PriceProductSchedule module.
    * Created PriceProductScheduleDataImportConfig.
    * Updated ConsoleDependencyProvider, DataImportDependencyProvider.
    * Added import file "product_price_schedule.csv".
    * Updated "config/Zed/cronjobs/jobs.php".

---
+++ src/Orm/Zed/PriceProductSchedule/Persistence/SpyPriceProductScheduleListQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\PriceProductSchedule\Persistence;
+
+use Spryker\Zed\PriceProductSchedule\Persistence\Propel\AbstractSpyPriceProductScheduleListQuery as BaseSpyPriceProductScheduleListQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_price_product_schedule_list' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyPriceProductScheduleListQuery extends BaseSpyPriceProductScheduleListQuery
+{
+
+}
