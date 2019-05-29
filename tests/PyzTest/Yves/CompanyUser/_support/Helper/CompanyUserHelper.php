3f4e4bfeca51d89add495363ce2a3a412994c3a4
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2019-05-29 12:54:52 +0300

    PS-9130: Created CompanyUser tests dir, Created empty CompanyUserRegistrationCest for future use, Introduced CompanyUserHelper, which encapsulates CompanyUser creation logic

---
+++ tests/PyzTest/Yves/CompanyUser/_support/Helper/CompanyUserHelper.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace PyzTest\Yves\CompanyUser\Helper;
+
+use Codeception\Module;
+use Codeception\Util\Stub;
+use Generated\Shared\Transfer\CompanyBusinessUnitTransfer;
+use Generated\Shared\Transfer\CompanyTransfer;
+use Generated\Shared\Transfer\CompanyUserTransfer;
+use Generated\Shared\Transfer\CustomerTransfer;
+use Spryker\Zed\CompanyMailConnector\CompanyMailConnectorDependencyProvider;
+use Spryker\Zed\CompanyMailConnector\Dependency\Facade\CompanyMailConnectorToMailFacadeBridge;
+use Spryker\Zed\Mail\Business\MailFacadeInterface;
+use SprykerTest\Shared\CompanyUser\Helper\CompanyUserHelper as SprykerTestCompanyUserHelper;
+use SprykerTest\Shared\Customer\Helper\CustomerDataHelper;
+use SprykerTest\Shared\Testify\Helper\DependencyHelperTrait;
+use SprykerTest\Zed\Company\Helper\CompanyHelper;
+ use SprykerTest\Zed\CompanyBusinessUnit\Helper\CompanyBusinessUnitHelper;
+
+class CompanyUserHelper extends Module
+{
+    use DependencyHelperTrait;
+
+    /**
+     * @return \Generated\Shared\Transfer\CompanyUserTransfer
+     */
+    public function haveRegisteredCompanyUser(): CompanyUserTransfer
+    {
+        $customerTransfer = $this->createCustomer();
+        $companyTransfer = $this->createCompany();
+
+        $companyBusinessUnitTransfer = $this->createCompanyBusinessUnit([
+            CompanyBusinessUnitTransfer::FK_COMPANY => $companyTransfer->getIdCompany(),
+        ]);
+        $companyUserTransfer = $this->createCompanyUser([
+            CompanyUserTransfer::CUSTOMER => $customerTransfer,
+            CompanyUserTransfer::FK_COMPANY => $companyTransfer->getIdCompany(),
+            CompanyUserTransfer::FK_COMPANY_BUSINESS_UNIT => $companyBusinessUnitTransfer->getIdCompanyBusinessUnit(),
+        ]);
+
+        // TODO: Remove if not needed.
+        $companyUserTransfer->getCustomer()
+            ->setCompanyUserTransfer($companyUserTransfer);
+
+        // TODO: Remove if not needed.
+        $companyUserTransfer->setCompany($companyTransfer)
+            ->setCompanyBusinessUnit($companyBusinessUnitTransfer);
+
+        return $companyUserTransfer;
+    }
+
+    /**
+     * @param array $seed
+     *
+     * @return \Generated\Shared\Transfer\CustomerTransfer
+     */
+    protected function createCustomer(array $seed = []): CustomerTransfer
+    {
+        return $this->getModule('\\' . CustomerDataHelper::class)->haveCustomer($seed + [
+                CustomerTransfer::IS_ENABLED => true,
+            ]);
+    }
+
+    /**
+     * @param array $seed
+     *
+     * @return \Generated\Shared\Transfer\CompanyTransfer
+     */
+    protected function createCompany(array $seed = []): CompanyTransfer
+    {
+        $mailMock = new CompanyMailConnectorToMailFacadeBridge($this->getMailMock());
+        $this->setDependency(CompanyMailConnectorDependencyProvider::FACADE_MAIL, $mailMock);
+
+        return $this->getModule('\\' . CompanyHelper::class)->haveCompany($seed + [
+                CompanyTransfer::IS_ACTIVE => true,
+                CompanyTransfer::STATUS => 'approved',
+            ]);
+    }
+
+    /**
+     * @param array $seed
+     *
+     * @return \Generated\Shared\Transfer\CompanyBusinessUnitTransfer
+     */
+    protected function createCompanyBusinessUnit(array $seed = []): CompanyBusinessUnitTransfer
+    {
+        return $this->getModule('\\' . CompanyBusinessUnitHelper::class)
+            ->haveCompanyBusinessUnit($seed);
+    }
+
+    /**
+     * @param array $seed
+     *
+     * @return \Generated\Shared\Transfer\CompanyUserTransfer
+     */
+    protected function createCompanyUser(array $seed = []): CompanyUserTransfer
+    {
+        return $this->getModule('\\' . SprykerTestCompanyUserHelper::class)
+            ->haveCompanyUser($seed + [
+                CompanyUserTransfer::IS_ACTIVE => true,
+            ]);
+    }
+
+    /**
+     * @return object|\Spryker\Zed\Mail\Business\MailFacadeInterface
+     */
+    protected function getMailMock()
+    {
+        return Stub::makeEmpty(MailFacadeInterface::class);
+    }
+}
