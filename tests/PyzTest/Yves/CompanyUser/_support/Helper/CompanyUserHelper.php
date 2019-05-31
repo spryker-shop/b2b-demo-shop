9b7eafb72471644b32e324889b4f59b30e0eeeb4
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2019-05-31 15:26:12 +0300

    PS-9130: Expanded CompanyUserHelper to provide initial permissions for newly created company users

--- tests/PyzTest/Yves/CompanyUser/_support/Helper/CompanyUserHelper.php
+++ tests/PyzTest/Yves/CompanyUser/_support/Helper/CompanyUserHelper.php
@@ -13,17 +13,28 @@ use Generated\Shared\Transfer\CompanyBusinessUnitTransfer;
 use Generated\Shared\Transfer\CompanyTransfer;
 use Generated\Shared\Transfer\CompanyUserTransfer;
 use Generated\Shared\Transfer\CustomerTransfer;
+use Generated\Shared\Transfer\PermissionCollectionTransfer;
+use Generated\Shared\Transfer\PermissionTransfer;
 use Spryker\Zed\CompanyMailConnector\CompanyMailConnectorDependencyProvider;
 use Spryker\Zed\CompanyMailConnector\Dependency\Facade\CompanyMailConnectorToMailFacadeBridge;
 use Spryker\Zed\Mail\Business\MailFacadeInterface;
 use SprykerTest\Shared\CompanyUser\Helper\CompanyUserHelper as SprykerTestCompanyUserHelper;
 use SprykerTest\Shared\Testify\Helper\DependencyHelperTrait;
+use SprykerTest\Shared\Testify\Helper\LocatorHelperTrait;
 use SprykerTest\Zed\Company\Helper\CompanyHelper;
- use SprykerTest\Zed\CompanyBusinessUnit\Helper\CompanyBusinessUnitHelper;
+use SprykerTest\Zed\CompanyBusinessUnit\Helper\CompanyBusinessUnitHelper;
 
 class CompanyUserHelper extends Module
 {
+    use LocatorHelperTrait;
     use DependencyHelperTrait;
+
+    protected const COMPANY_USER_PERMISSIONS_KEY_LIST = [
+        'AddCartItemPermissionPlugin',
+        'ChangeCartItemPermissionPlugin',
+        'RemoveCartItemPermissionPlugin',
+        'AlterCartUpToAmountPermissionPlugin',
+    ];
 
     /**
      * @param \Generated\Shared\Transfer\CustomerTransfer $customerTransfer
@@ -43,7 +54,71 @@ class CompanyUserHelper extends Module
             CompanyUserTransfer::FK_COMPANY_BUSINESS_UNIT => $companyBusinessUnitTransfer->getIdCompanyBusinessUnit(),
         ]);
 
+        $this->updateCompanyUserRolePermissions($companyUserTransfer);
+
         return $companyUserTransfer;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CompanyUserTransfer $companyUserTransfer
+     *
+     * @return \Generated\Shared\Transfer\CompanyUserTransfer
+     */
+    protected function updateCompanyUserRolePermissions(CompanyUserTransfer $companyUserTransfer): CompanyUserTransfer
+    {
+        $companyRoleTransfer = $companyUserTransfer->getCompanyRoleCollection()
+            ->getRoles()
+            ->offsetGet(0);
+
+        $companyRoleTransfer->setPermissionCollection(
+            $this->getUpdatedPermissionCollectionTransfer($companyRoleTransfer->getPermissionCollection())
+        );
+
+        $this->getCompanyRoleFacade()->update($companyRoleTransfer);
+
+        return $companyUserTransfer;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\PermissionCollectionTransfer $companyRolePermissionCollectionTransfer
+     *
+     * @return \Generated\Shared\Transfer\PermissionCollectionTransfer
+     */
+    protected function getUpdatedPermissionCollectionTransfer(
+        PermissionCollectionTransfer $companyRolePermissionCollectionTransfer
+    ): PermissionCollectionTransfer {
+        $availablePermissionCollectionTransfer = $this->getPermissionFacade()
+            ->findMergedRegisteredNonInfrastructuralPermissions();
+
+        foreach ($availablePermissionCollectionTransfer->getPermissions() as $permissionTransfer) {
+            if (in_array($permissionTransfer->getKey(), static::COMPANY_USER_PERMISSIONS_KEY_LIST, true)) {
+                $companyRolePermissionCollectionTransfer = $this->addPermissionToPermissionCollection(
+                    $permissionTransfer,
+                    $companyRolePermissionCollectionTransfer
+                );
+            }
+        }
+
+        return $companyRolePermissionCollectionTransfer;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\PermissionTransfer $permissionTransfer
+     * @param \Generated\Shared\Transfer\PermissionCollectionTransfer $permissionCollectionTransfer
+     *
+     * @return \Generated\Shared\Transfer\PermissionCollectionTransfer
+     */
+    protected function addPermissionToPermissionCollection(
+        PermissionTransfer $permissionTransfer,
+        PermissionCollectionTransfer $permissionCollectionTransfer
+    ): PermissionCollectionTransfer {
+        foreach ($permissionCollectionTransfer as $companyRolePermissionTransfer) {
+            if ($companyRolePermissionTransfer->getIdPermission() === $permissionTransfer->getIdPermission()) {
+                return $permissionCollectionTransfer;
+            }
+        }
+
+        return $permissionCollectionTransfer->addPermission($permissionTransfer);
     }
 
     /**
@@ -84,6 +159,22 @@ class CompanyUserHelper extends Module
             ->haveCompanyUser($seed + [
                 CompanyUserTransfer::IS_ACTIVE => true,
             ]);
+    }
+
+    /**
+     * @return \Spryker\Zed\CompanyRole\Business\CompanyRoleFacadeInterface
+     */
+    protected function getCompanyRoleFacade()
+    {
+        return $this->getLocator()->companyRole()->facade();
+    }
+
+    /**
+     * @return \Spryker\Zed\Permission\Business\PermissionFacadeInterface
+     */
+    protected function getPermissionFacade()
+    {
+        return $this->getLocator()->permission()->facade();
     }
 
     /**
