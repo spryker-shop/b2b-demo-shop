8006c803099763a97cb4a29b8afd8700e39da107
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2019-06-07 09:50:58 +0300

    cc-2363 add gloassary key

---
+++ src/Pyz/Zed/PriceProductStorage/PriceProductStorageConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kravchenko
+ * Date: 2019-06-06
+ * Time: 17:20
+ */
+
+namespace Pyz\Zed\PriceProductStorage;
+
+
+class PriceProductStorageConfig
+{
+
+}
