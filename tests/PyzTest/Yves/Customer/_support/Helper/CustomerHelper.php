d87eb9d5593ce6303780e8ab8c56bc53f6f28c3a
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2019-06-12 14:57:35 +0300

    Added wait for customer login test helper

--- tests/PyzTest/Yves/Customer/_support/Helper/CustomerHelper.php
+++ tests/PyzTest/Yves/Customer/_support/Helper/CustomerHelper.php
@@ -137,6 +137,7 @@ class CustomerHelper extends Module
 
         $tester = $this->getWebDriver();
         $tester->amOnPage(CustomerLoginPage::URL);
+        $tester->wait(2);
         $tester->submitForm(['name' => 'loginForm'], [
             CustomerLoginPage::FORM_FIELD_SELECTOR_EMAIL => $customerTransfer->getEmail(),
             CustomerLoginPage::FORM_FIELD_SELECTOR_PASSWORD => $customerTransfer->getPassword(),
