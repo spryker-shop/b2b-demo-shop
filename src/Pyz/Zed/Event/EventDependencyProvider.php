1abea7d9c1fc63fbb7cc171b3eb0dbf3c1e79844
Aleksandr Kovalchuk <avtop25495@gmail.com>
2019-06-20 16:28:47 +0300

    GLUE-8174 integarated taxsets

--- src/Pyz/Zed/Event/EventDependencyProvider.php
+++ src/Pyz/Zed/Event/EventDependencyProvider.php
@@ -51,6 +51,8 @@ use Spryker\Zed\ProductSetPageSearch\Communication\Plugin\Event\Subscriber\Produ
 use Spryker\Zed\ProductSetStorage\Communication\Plugin\Event\Subscriber\ProductSetStorageEventSubscriber;
 use Spryker\Zed\ProductStorage\Communication\Plugin\Event\Subscriber\ProductStorageEventSubscriber;
 use Spryker\Zed\ShoppingListStorage\Communication\Plugin\Event\Subscriber\ShoppingListStorageEventSubscriber;
+use Spryker\Zed\TaxProductStorage\Communication\Plugin\Event\Subscriber\TaxProductStorageSubscriber;
+use Spryker\Zed\TaxStorage\Communication\Plugin\Event\Subscriber\TaxStorageSubscriber;
 use Spryker\Zed\UrlStorage\Communication\Plugin\Event\Subscriber\UrlStorageEventSubscriber;
 
 class EventDependencyProvider extends SprykerEventDependencyProvider
@@ -107,6 +109,8 @@ class EventDependencyProvider extends SprykerEventDependencyProvider
         $eventSubscriberCollection->add(new CategoryImageStorageEventSubscriber());
         $eventSubscriberCollection->add(new AvailabilityNotificationSubscriber());
         $eventSubscriberCollection->add(new CompanyUserStorageEventSubscriber());
+        $eventSubscriberCollection->add(new TaxStorageSubscriber());
+        $eventSubscriberCollection->add(new TaxProductStorageSubscriber());
 
         /**
          * Search Events
---
+++ src/Pyz/Zed/TaxProductStorage/TaxProductStorageConfig.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\TaxProductStorage;
+
+use Pyz\Zed\Synchronization\SynchronizationConfig;
+use Spryker\Zed\TaxProductStorage\TaxProductStorageConfig as SprykerTaxProductStorageConfig;
+
+class TaxProductStorageConfig extends SprykerTaxProductStorageConfig
+{
+    /**
+     * @return string|null
+     */
+    public function getTaxProductSynchronizationPoolName(): ?string
+    {
+        return SynchronizationConfig::DEFAULT_SYNCHRONIZATION_POOL_NAME;
+    }
+}
