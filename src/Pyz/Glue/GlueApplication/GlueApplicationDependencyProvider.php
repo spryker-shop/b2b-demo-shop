860c4ce8903febdc83955c4a4ed12eb980c7d879
AntonKhabiuk <brotherosirus@gmail.com>
2019-07-22 15:36:11 +0300

    GLUE-8446 Add missing plugin

--- src/Pyz/Glue/GlueApplication/GlueApplicationDependencyProvider.php
+++ src/Pyz/Glue/GlueApplication/GlueApplicationDependencyProvider.php
@@ -97,6 +97,7 @@ use Spryker\Glue\ProductTaxSetsRestApi\Plugin\GlueApplication\ProductTaxSetsReso
 use Spryker\Glue\RelatedProductsRestApi\Plugin\GlueApplication\RelatedProductsResourceRoutePlugin;
 use Spryker\Glue\RestRequestValidator\Plugin\ValidateRestRequestAttributesPlugin;
 use Spryker\Glue\SharedCartsRestApi\Plugin\GlueApplication\SharedCartByCartIdResourceRelationshipPlugin;
+use Spryker\Glue\SharedCartsRestApi\Plugin\GlueApplication\SharedCartsResourceRoutePlugin;
 use Spryker\Glue\SharedCartsRestApi\SharedCartsRestApiConfig;
 use Spryker\Glue\StoresRestApi\Plugin\StoresResourceRoutePlugin;
 use Spryker\Glue\UpSellingProductsRestApi\Plugin\GlueApplication\CartUpSellingProductsResourceRoutePlugin;
@@ -153,6 +154,7 @@ class GlueApplicationDependencyProvider extends SprykerGlueApplicationDependency
             new ProductTaxSetsResourceRoutePlugin(),
             new CartPermissionGroupsResourceRoutePlugin(),
             new OrderPaymentsResourceRoutePlugin(),
+            new SharedCartsResourceRoutePlugin(),
         ];
     }
 
