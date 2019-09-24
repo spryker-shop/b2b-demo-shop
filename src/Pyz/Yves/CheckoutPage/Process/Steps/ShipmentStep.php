b6c49485acece1439c156880ad2c0b10dc045f79
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-09-24 15:35:34 +0300

    CC-6571. Providing of additional data from BE for FE templates
    * Created `Pyz\Yves\CheckoutPage\Process\Steps\ShipmentStep`.
    * Created `Pyz\Yves\CheckoutPage\Process\StepFactory`.
    * Updated `Pyz\Yves\CheckoutPage\CheckoutPageFactory`.

---
+++ src/Pyz/Yves/CheckoutPage/Process/Steps/ShipmentStep.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Yves\CheckoutPage\Process\Steps;
+
+use Spryker\Shared\Kernel\Store;
+use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
+use Spryker\Yves\StepEngine\Dependency\Plugin\Handler\StepHandlerPluginCollection;
+use SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToCalculationClientInterface;
+use SprykerShop\Yves\CheckoutPage\GiftCard\GiftCardItemsCheckerInterface;
+use SprykerShop\Yves\CheckoutPage\Process\Steps\PostConditionCheckerInterface;
+use SprykerShop\Yves\CheckoutPage\Process\Steps\ShipmentStep as SprykerShipmentStep;
+
+class ShipmentStep extends SprykerShipmentStep
+{
+    /**
+     * @var \SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToCalculationClientInterface
+     */
+    protected $store;
+
+    /**
+     * @param \SprykerShop\Yves\CheckoutPage\Dependency\Client\CheckoutPageToCalculationClientInterface $calculationClient
+     * @param \Spryker\Yves\StepEngine\Dependency\Plugin\Handler\StepHandlerPluginCollection $shipmentPlugins
+     * @param \SprykerShop\Yves\CheckoutPage\Process\Steps\PostConditionCheckerInterface $postConditionChecker
+     * @param \SprykerShop\Yves\CheckoutPage\GiftCard\GiftCardItemsCheckerInterface $giftCardItemsChecker
+     * @param \Spryker\Shared\Kernel\Store $store
+     * @param string $stepRoute
+     * @param string $escapeRoute
+     */
+    public function __construct(
+        CheckoutPageToCalculationClientInterface $calculationClient,
+        StepHandlerPluginCollection $shipmentPlugins,
+        PostConditionCheckerInterface $postConditionChecker,
+        GiftCardItemsCheckerInterface $giftCardItemsChecker,
+        Store $store,
+        $stepRoute,
+        $escapeRoute
+    ) {
+        parent::__construct(
+            $calculationClient,
+            $shipmentPlugins,
+            $postConditionChecker,
+            $giftCardItemsChecker,
+            $stepRoute,
+            $escapeRoute
+        );
+
+        $this->store = $store;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return array
+     */
+    public function getTemplateVariables(AbstractTransfer $quoteTransfer)
+    {
+        return [
+            'currentLanguage' => $this->store->getCurrentLanguage(),
+        ];
+    }
+}
