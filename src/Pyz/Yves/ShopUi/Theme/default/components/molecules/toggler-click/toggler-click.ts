dfb49917c04a46bf19d45a30ed2dfd7183f53b82
k.kravchenko <kostiantyn.Kravchenko@spryker.com>
2019-10-01 09:26:33 +0300

    PS-10835 Refactoring toggler-click component.

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/toggler-click/toggler-click.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/toggler-click/toggler-click.ts
@@ -1,15 +1,9 @@
-import Component from 'ShopUi/models/component';
+import TogglerClickCore from 'ShopUi/components/molecules/toggler-click/toggler-click';
 
-export default class TogglerClick extends Component {
-    readonly triggers: HTMLElement[];
-    readonly targets: HTMLElement[];
+export default class TogglerClick extends TogglerClickCore {
     protected disablers: HTMLElement[] = [];
 
-    constructor() {
-        super();
-        this.triggers = <HTMLElement[]>Array.from(document.querySelectorAll(this.triggerSelector));
-        this.targets = <HTMLElement[]>Array.from(document.querySelectorAll(this.targetSelector));
-
+    protected init(): void {
         if (this.disablerClassName) {
             const disablerClassNamesList = this.disablerClassName.split(',');
 
@@ -18,26 +12,16 @@ export default class TogglerClick extends Component {
                     [...this.disablers, ...Array.from(document.getElementsByClassName(disablerClassName))];
             });
         }
-    }
 
-    protected readyCallback(): void {
-        this.mapEvents();
+        super.init();
     }
 
     protected mapEvents(): void {
-        this.triggers.forEach((trigger: HTMLElement) => {
-            trigger.addEventListener('click', (event: Event) => this.onTriggerClick(event));
-        });
+        super.mapEvents();
 
         this.disablers.forEach((disabler: HTMLElement) => {
             disabler.addEventListener('click', (event: Event) => this.removeClassToToggle());
         });
-    }
-
-    protected onTriggerClick(event: Event): void {
-        event.preventDefault();
-
-        this.toggle(event);
     }
 
     protected removeClassToToggle(): void {
@@ -46,36 +30,23 @@ export default class TogglerClick extends Component {
         });
     }
 
-    toggle(event: Event): void {
+    toggle(): void {
         this.onTriggerToggleClass(event);
 
-        this.targets.forEach((target: HTMLElement) => {
-            const addClass = !target.classList.contains(this.classToToggle);
-            target.classList.toggle(this.classToToggle, addClass);
-        });
+        super.toggle();
     }
 
     protected onTriggerToggleClass(event: Event): void {
-        if (this.triggerClassToToggle.length !== 0) {
-            const triggerTarget = <HTMLElement>event.currentTarget;
-
-            triggerTarget.classList.toggle(this.triggerClassToToggle);
+        if (!this.triggerClassToToggle.length) {
+            return;
         }
+
+        const triggerTarget = <HTMLElement>event.currentTarget;
+
+        triggerTarget.classList.toggle(this.triggerClassToToggle);
     }
 
-    get triggerSelector(): string {
-        return this.getAttribute('trigger-selector');
-    }
-
-    get targetSelector(): string {
-        return this.getAttribute('target-selector');
-    }
-
-    get classToToggle(): string {
-        return this.getAttribute('class-to-toggle');
-    }
-
-    get triggerClassToToggle(): string {
+    protected get triggerClassToToggle(): string {
         return this.getAttribute('trigger-class-to-toggle');
     }
 
