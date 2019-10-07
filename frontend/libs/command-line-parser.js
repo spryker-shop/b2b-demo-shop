6d65269ac5e2b35e427e8aad092081a50b42a758
k.kravchenko <kostiantyn.Kravchenko@spryker.com>
2019-10-07 14:28:26 +0300

    PS-11013 Integrating frontend command line parser

--- frontend/libs/command-line-parser.js
+++ frontend/libs/command-line-parser.js
@@ -1,23 +1,92 @@
 const commandLineParser = require('commander');
 const { join } = require('path');
 const { globalSettings } = require('../settings');
+const { scripts } = require('../../package.json');
+let mode = null;
 
 const collectArguments = (argument, argumentCollection) => {
     argumentCollection.push(argument);
     return argumentCollection;
 };
 
-const getMode = requestedMode => {
-    for (const mode in globalSettings.modes) {
-        if (globalSettings.modes[mode] === requestedMode) {
-            return requestedMode;
-        }
+const validateMode = requestedMode => {
+    const { modes } = globalSettings;
+    const isValidMode = Object.values(modes).find(mode => mode === requestedMode);
+
+    if (!isValidMode) {
+        throw new Error(`Mode "${requestedMode}" is not available`);
     }
-    console.warn(`Mode "${commandLineParser.mode}" is not available`);
-    process.exit(1);
 };
 
-let mode;
+const getAllowedFlagsData = (commandLineParserInfo, configData) => {
+    const allowedFlagsData = {};
+
+    commandLineParserInfo.options.forEach(option => {
+        const {short: shortFlagName, long: longFlagName, required: isFlagValueRequired} = option;
+
+        allowedFlagsData[shortFlagName] = {
+            required: isFlagValueRequired,
+        };
+
+        allowedFlagsData[longFlagName] = {
+            required: isFlagValueRequired,
+        };
+    });
+
+    Object.values(configData).forEach(value => {
+        const flagsData = value.match(/--[a-z]{1,}/g);
+
+        if (flagsData) {
+            flagsData.forEach(flag => {
+                allowedFlagsData[flag] = {
+                    required: false,
+                };
+            })
+        }
+    });
+
+    return allowedFlagsData;
+};
+
+const validateFlag = (flag, allowedFlagsData) => {
+    if (!flag.indexOf('-') && !allowedFlagsData[flag]) {
+        throw new Error(`Flag "${flag}" is not available`);
+    }
+};
+
+const isCommand = (allowedFlagsData, args, index) => {
+    const previousParameter = args[index - 1];
+    const currentParameter = args[index];
+    const isFlag = !currentParameter.indexOf('-');
+    const isFlagValue = !previousParameter.indexOf('-') && allowedFlagsData[previousParameter].required;
+    const isValidCommand = !!scripts[currentParameter] || currentParameter === 'node';
+
+    if (isFlag || isFlagValue) {
+        return false;
+    };
+
+    if (isValidCommand) {
+        return true;
+    };
+
+    throw new Error(`Command "${args[index]}" is not available`);
+}
+
+const validateParameters = env => {
+    if (!env || !env.npm_config_argv) {
+        return;
+    }
+
+    const originalArgumentsString = env.npm_config_argv;
+    const {original: originalArguments} = JSON.parse(originalArgumentsString);
+
+    originalArguments.forEach(argument => {
+        if (!argument.indexOf('-') && !originalArguments.includes('--')) {
+            throw new Error('It is impossible to use flags without "--" indentifier if you use "npm" script.');
+        }
+    });
+};
+
 commandLineParser
     .option('-n, --namespace <namespace name>', 'build the requested namespace. Multiple arguments are allowed.', collectArguments, [])
     .option('-t, --theme <theme name>', 'build the requested theme. Multiple arguments are allowed.', collectArguments, [])
@@ -25,7 +94,26 @@ commandLineParser
     .option('-c, --config <path>', 'path to JSON file with namespace config', globalSettings.paths.namespaceConfig)
     .arguments('<mode>')
     .action(function (modeValue) {
-        mode = getMode(modeValue);
+        const { argv, env } = process;
+        const modeIndexInArgs = process.argv.findIndex(element => element === modeValue);
+        const allowedFlagsData = getAllowedFlagsData(this, scripts);
+
+        validateParameters(env);
+
+        argv.forEach((arg, index) => {
+            if (index <= modeIndexInArgs) {
+                return;
+            };
+
+            validateMode(modeValue);
+            validateFlag(arg, allowedFlagsData);
+
+            if (isCommand(allowedFlagsData, argv, index)) {
+                console.warn('It is impossible to use several commands. All commands and parameters entered after the second command are ignored.');
+            }
+        });
+
+        mode = modeValue;
     })
     .parse(process.argv);
 
