2b7d0f4d51643c3a7eed0d859fc4d68767a2110e
k.kravchenko <kostiantyn.Kravchenko@spryker.com>
2019-11-07 15:27:51 +0200

    PS-11318: Refactoring after code review

--- src/Pyz/Yves/SalesConfigurableBundleWidget/Theme/default/components/molecules/bundle-checkbox/bundle-checkbox.ts
+++ src/Pyz/Yves/SalesConfigurableBundleWidget/Theme/default/components/molecules/bundle-checkbox/bundle-checkbox.ts
@@ -2,34 +2,46 @@ import Component from 'ShopUi/models/component';
 
 export default class BundleCheckbox extends Component {
     protected triggers: HTMLElement[];
+    protected parents: HTMLElement[];
 
     protected readyCallback(): void {}
 
     protected init(): void {
         this.triggers = <HTMLElement[]>Array.from(document.getElementsByClassName(this.triggerClassName));
+        this.parents = <HTMLElement[]>Array.from(document.getElementsByClassName(this.parentClassName))
 
+        this.addUniqueClasses();
         this.mapEvents();
     }
 
     protected mapEvents(): void {
         this.triggers.forEach((trigger: HTMLInputElement) => {
-            trigger.getElementsByTagName('input')[0]
-                .addEventListener('change', (event: Event) => this.onTriggerChange(event));
+            trigger.addEventListener('change', (event: Event) => this.onTriggerChange(event));
+        });
+    }
+
+    protected addUniqueClasses(): void {
+        this.parents.forEach((parent: HTMLElement, index: number) => {
+            Array.from(parent.getElementsByClassName(this.targetClassName)).forEach((checkbox: HTMLInputElement) => {
+                const uniqueClass = `${this.targetClassName}${index}`;
+
+                if (checkbox.classList.contains(this.triggerClassName)) {
+                    checkbox.dataset.target = uniqueClass;
+                }
+
+                if (!checkbox.classList.contains(this.triggerClassName)) {
+                    checkbox.classList.add(uniqueClass);
+                }
+            });
         });
     }
 
     protected onTriggerChange(event: Event): void {
         const target = <HTMLInputElement>event.target;
-        this.toggle(target);
-    }
+        const status = <boolean>target.checked;
 
-    protected toggle(checkbox: HTMLInputElement): void {
-        const status = <boolean>checkbox.checked;
-        const parent = <HTMLElement>checkbox.closest(`.${this.parentClassName}`);
-        const inputs = <HTMLElement[]>Array.from(parent.getElementsByClassName(this.targetClassName));
-
-        inputs.forEach((target: HTMLInputElement) => {
-            target.checked = status;
+        Array.from(document.getElementsByClassName(target.dataset.target)).forEach((input: HTMLInputElement) => {
+            input.checked = status;
         });
     }
 
