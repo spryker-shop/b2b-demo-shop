efeb9f8ffd87a8a7c7b4c13d7f474f44a2e92bfd
Daniel Kleba <danylo.kleba@spryker.com>
2019-11-18 13:42:09 +0000

    CC-6706: Codesniffer fixes.

--- src/Pyz/Zed/ShipmentDataImport/ShipmentDataImportConfig.php
+++ src/Pyz/Zed/ShipmentDataImport/ShipmentDataImportConfig.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Pyz\Zed\ShipmentDataImport;
 
 use Generated\Shared\Transfer\DataImporterConfigurationTransfer;
