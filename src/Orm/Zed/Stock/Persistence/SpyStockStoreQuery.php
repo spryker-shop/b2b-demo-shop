c93d7095f63b644693eb1e2b70924024301ff8d0
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-11-29 16:04:55 +0200

    CC-7385. Decimal Stock integration into the b2b. BE part.
    * Updated `DataImportDependencyProvider`.

--- src/Orm/Zed/Stock/Persistence/SpyStockStoreQuery.php
+++ src/Orm/Zed/Stock/Persistence/SpyStockStoreQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\Stock\Persistence;
 
 use Spryker\Zed\Stock\Persistence\Propel\AbstractSpyStockStoreQuery as BaseSpyStockStoreQuery;
@@ -10,11 +15,9 @@ use Spryker\Zed\Stock\Persistence\Propel\AbstractSpyStockStoreQuery as BaseSpySt
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
- *
  */
 class SpyStockStoreQuery extends BaseSpyStockStoreQuery
 {
-
 }
