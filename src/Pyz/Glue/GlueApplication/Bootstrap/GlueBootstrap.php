1ef892227e8eb3ed2865ae11fee26bcda7fccf95
gechetspr <serhii.chepela@spryker.com>
2019-12-05 17:20:15 +0200

    TE-4528 removig deprecated providers for glue

--- src/Pyz/Glue/GlueApplication/Bootstrap/GlueBootstrap.php
+++ src/Pyz/Glue/GlueApplication/Bootstrap/GlueBootstrap.php
@@ -7,26 +7,8 @@
 
 namespace Pyz\Glue\GlueApplication\Bootstrap;
 
-use Silex\Provider\ServiceControllerServiceProvider;
-use Silex\Provider\SessionServiceProvider;
 use Spryker\Glue\GlueApplication\Bootstrap\AbstractGlueBootstrap;
-use Spryker\Glue\GlueApplication\Plugin\Rest\GlueServiceProviderPlugin;
-use Spryker\Glue\GlueApplication\Plugin\Rest\ServiceProvider\GlueApplicationServiceProvider;
-use Spryker\Glue\GlueApplication\Plugin\Rest\ServiceProvider\GlueResourceBuilderService;
-use Spryker\Glue\GlueApplication\Plugin\Rest\ServiceProvider\GlueRoutingServiceProvider;
 
 class GlueBootstrap extends AbstractGlueBootstrap
 {
-    /**
-     * @return void
-     */
-    protected function registerServiceProviders(): void
-    {
-        $this->application
-            ->register(new GlueResourceBuilderService())
-            ->register(new GlueApplicationServiceProvider())
-            ->register(new ServiceControllerServiceProvider())
-            ->register(new GlueServiceProviderPlugin())
-            ->register(new GlueRoutingServiceProvider());
-    }
 }
