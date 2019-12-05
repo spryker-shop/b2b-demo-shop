625b8a029a8974e2b8edc19780bb33a48e010ae9
danil.moroz <danil.moroz@spryker.com>
2019-12-05 12:08:28 +0200

    CC-7557: FE. Refactoring after code review. Round 1 [Gerton]

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/quantity-counter/quantity-counter.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/quantity-counter/quantity-counter.ts
@@ -34,8 +34,8 @@ export default class QuantityCounter extends Component {
         event.preventDefault();
         if (this.isAvailable) {
             const value = Number(this.input.value);
-            const potentialValue = Number(((value * this.precision) + (this.step * this.precision)) /
-                this.precision).toFixed(this.numberOfDecimalPlaces);
+            const potentialValue = Number((((value * this.precision) + (this.step * this.precision)) /
+                this.precision).toFixed(this.numberOfDecimalPlaces));
 
             if (value < this.maxQuantity) {
                 this.input.value = potentialValue.toString();
@@ -48,10 +48,10 @@ export default class QuantityCounter extends Component {
         event.preventDefault();
         if (this.isAvailable) {
             const value = Number(this.input.value);
-            const potentialValue = Number(((value * this.precision) - (this.step * this.precision)) /
-                this.precision).toFixed(this.numberOfDecimalPlaces);
+            const potentialValue = Number((((value * this.precision) - (this.step * this.precision)) /
+                this.precision).toFixed(this.numberOfDecimalPlaces));
 
-            if (Number(potentialValue) >= this.minQuantity) {
+            if (potentialValue >= this.minQuantity) {
                 this.input.value = potentialValue.toString();
                 this.triggerInputEvent();
             }
