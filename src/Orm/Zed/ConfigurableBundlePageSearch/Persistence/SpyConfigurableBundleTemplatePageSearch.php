4773a385550fcc91278ff96ac166f4e4d8cedb06
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-12-06 12:39:42 +0200

    CC-7346. Decimal Stock integration into the b2b. BE part.
    * Updated `product_image.csv` file.

---
+++ src/Orm/Zed/ConfigurableBundlePageSearch/Persistence/SpyConfigurableBundleTemplatePageSearch.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ConfigurableBundlePageSearch\Persistence;
+
+use Spryker\Zed\ConfigurableBundlePageSearch\Persistence\Propel\AbstractSpyConfigurableBundleTemplatePageSearch as BaseSpyConfigurableBundleTemplatePageSearch;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_configurable_bundle_template_page_search' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyConfigurableBundleTemplatePageSearch extends BaseSpyConfigurableBundleTemplatePageSearch
+{
+
+}
