4e4ce7428d68f0628c596f9ac2364ecb277f2ecb
gechetspr <serhii.chepela@spryker.com>
2019-12-06 10:33:45 +0200

    TE-4528 fixed priceMode issue

--- src/Pyz/Yves/ShopApplication/YvesBootstrap.php
+++ src/Pyz/Yves/ShopApplication/YvesBootstrap.php
@@ -7,8 +7,17 @@
 
 namespace Pyz\Yves\ShopApplication;
 
+use Pyz\Yves\PriceWidget\Plugin\Provider\TwigPriceModeFunctionServiceProvider;
 use SprykerShop\Yves\ShopApplication\YvesBootstrap as SprykerYvesBootstrap;
 
 class YvesBootstrap extends SprykerYvesBootstrap
 {
+
+    /**
+     * @return void
+     */
+    protected function registerServiceProviders()
+    {
+        $this->application->register(new TwigPriceModeFunctionServiceProvider());
+    }
 }
