7833b95f970180f59ab3b7919c572e91d61ac9cd
Aleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2020-01-22 13:45:54 +0200

    Update config_default-docker.php
    Adjust docker configuration

--- config/Shared/config_default-docker.php
+++ config/Shared/config_default-docker.php
@@ -675,7 +675,4 @@ $config[ConsoleConstants::ENABLE_DEVELOPMENT_CONSOLE_COMMANDS] = (bool)getenv('D
 $config[DocumentationGeneratorRestApiConstants::ENABLE_REST_API_DOCUMENTATION_GENERATION] = true;
 
 // ----------- HTTP Security
-$config[KernelConstants::DOMAIN_WHITELIST] = [
-    $config[ApplicationConstants::HOST_YVES],
-    $config[ApplicationConstants::HOST_ZED],
-];
+$config[KernelConstants::DOMAIN_WHITELIST] = [];
