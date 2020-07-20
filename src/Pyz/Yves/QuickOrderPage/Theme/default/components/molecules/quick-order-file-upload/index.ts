0233b0ec2fd4db56279bed674f7015c0e14a4bb6
Yuriy Gerton <yuriy.gerton@spryker.com>
2020-07-20 16:15:40 +0300

    TE-6430: change import type for quick-order-file-upload

--- src/Pyz/Yves/QuickOrderPage/Theme/default/components/molecules/quick-order-file-upload/index.ts
+++ src/Pyz/Yves/QuickOrderPage/Theme/default/components/molecules/quick-order-file-upload/index.ts
@@ -1,3 +1,3 @@
 import './quick-order-file-upload.scss';
 import register from 'ShopUi/app/registry';
-export default register('quick-order-file-upload', () => import(/* webpackMode: "lazy" */'./quick-order-file-upload'));
+export default register('quick-order-file-upload', () => import(/* webpackMode: "eager" */'./quick-order-file-upload'));
