27da7b68bf82c3e067156ca35de9c0ad9a4da523
galitsky <eugen.galitsky@spryker.com>
2020-07-21 16:41:31 +0300

    TE-6548: remove remote form submit styles, fix quote duplicate button

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/remote-form-submit/index.ts
+++
@@ -1,6 +0,0 @@
-import './remote-form-submit.scss';
-import register from 'ShopUi/app/registry';
-export default register(
-    'remote-form-submit',
-    () => import(/* webpackMode: "lazy" */'ShopUi/components/molecules/remote-form-submit/remote-form-submit')
-);
