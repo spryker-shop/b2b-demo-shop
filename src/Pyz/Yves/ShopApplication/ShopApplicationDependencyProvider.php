5f93ad4dfbdb8e84d31bb715f736e5c35307a183
Anton Zubariev <anton.zubariev@spryker.com>
2020-07-22 11:10:50 +0300

    [PS-13841] B2B Add to cart form the catalog page integration

--- src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
+++ src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
@@ -33,6 +33,7 @@ use SprykerShop\Yves\CartNoteWidget\Widget\CartNoteFormWidget;
 use SprykerShop\Yves\CartPage\Widget\AddItemsFormWidget;
 use SprykerShop\Yves\CartPage\Widget\AddToCartFormWidget;
 use SprykerShop\Yves\CartPage\Widget\CartChangeQuantityFormWidget;
+use SprykerShop\Yves\CartPage\Widget\ProductAbstractAddToCartButtonWidget;
 use SprykerShop\Yves\CartPage\Widget\RemoveFromCartFormWidget;
 use SprykerShop\Yves\CategoryImageStorageWidget\Widget\CategoryImageStorageWidget;
 use SprykerShop\Yves\CheckoutWidget\Widget\CheckoutBreadcrumbWidget;
@@ -246,6 +247,7 @@ class ShopApplicationDependencyProvider extends SprykerShopApplicationDependency
             AddItemsFormWidget::class,
             CartChangeQuantityFormWidget::class,
             RemoveFromCartFormWidget::class,
+            ProductAbstractAddToCartButtonWidget::class,
         ];
     }
 
