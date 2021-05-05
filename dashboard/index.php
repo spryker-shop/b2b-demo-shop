efb3a883d992092698f558d7696bb3fabd5304bb
Ehsan Zanjani <ehsan.zanjani@gmail.com>
2021-05-05 09:05:10 +0000

    Clean up

--- dashboard/index.php
+++ dashboard/index.php
@@ -68,7 +68,7 @@ $result = $conn->query($sql);
   <body>
 
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Antilope Delivery Panel (Branch #2637)</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Delivery Panel (Driver-2637)</a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -84,7 +84,7 @@ $result = $conn->query($sql);
   <div class="row">
     <main class="col-md-12 ms-sm-auto col-lg-12 px-md-12">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Active Orders</h1>
+        <h1 class="h2">Active Deliveries</h1>
           <img src="delivery.jpeg" style="width: 150px"/>
       </div>
       <div class="table-responsive">
