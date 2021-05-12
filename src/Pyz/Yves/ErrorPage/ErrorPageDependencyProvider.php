590f80aae2fa2be3eb11a3a6b912c98fd2a6c5ee
pavel.makismov <pavlo.maksymov@spryker.com>
2021-05-12 16:53:48 +0300

    CC-13219 - implemented RedirectExceptionHandlerPlugin

--- src/Pyz/Yves/ErrorPage/ErrorPageDependencyProvider.php
+++ src/Pyz/Yves/ErrorPage/ErrorPageDependencyProvider.php
@@ -8,16 +8,18 @@
 namespace Pyz\Yves\ErrorPage;
 
 use SprykerShop\Yves\ErrorPage\ErrorPageDependencyProvider as SprykerErrorPageDependencyProvider;
+use SprykerShop\Yves\ErrorPage\Plugin\ExceptionHandler\RedirectExceptionHandlerPlugin;
 use SprykerShop\Yves\ErrorPage\Plugin\ExceptionHandler\SubRequestExceptionHandlerPlugin;
 
 class ErrorPageDependencyProvider extends SprykerErrorPageDependencyProvider
 {
     /**
-     * @return \SprykerShop\Yves\ErrorPage\Dependency\Plugin\ExceptionHandlerPluginInterface[]
+     * @return \SprykerShop\Yves\ErrorPageExtension\Dependency\Plugin\ExceptionHandlerPluginInterface[]
      */
     protected function getExceptionHandlerPlugins(): array
     {
         return [
+            new RedirectExceptionHandlerPlugin(),
             new SubRequestExceptionHandlerPlugin(),
         ];
     }
