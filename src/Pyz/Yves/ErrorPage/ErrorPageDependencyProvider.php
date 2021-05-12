f216301927a932394001baa46902610ca2b41eb5
pavel.makismov <pavlo.maksymov@spryker.com>
2021-05-12 16:56:38 +0300

    Revert "CC-13219 - implemented RedirectExceptionHandlerPlugin"
    This reverts commit 590f80aa

--- src/Pyz/Yves/ErrorPage/ErrorPageDependencyProvider.php
+++ src/Pyz/Yves/ErrorPage/ErrorPageDependencyProvider.php
@@ -8,18 +8,16 @@
 namespace Pyz\Yves\ErrorPage;
 
 use SprykerShop\Yves\ErrorPage\ErrorPageDependencyProvider as SprykerErrorPageDependencyProvider;
-use SprykerShop\Yves\ErrorPage\Plugin\ExceptionHandler\RedirectExceptionHandlerPlugin;
 use SprykerShop\Yves\ErrorPage\Plugin\ExceptionHandler\SubRequestExceptionHandlerPlugin;
 
 class ErrorPageDependencyProvider extends SprykerErrorPageDependencyProvider
 {
     /**
-     * @return \SprykerShop\Yves\ErrorPageExtension\Dependency\Plugin\ExceptionHandlerPluginInterface[]
+     * @return \SprykerShop\Yves\ErrorPage\Dependency\Plugin\ExceptionHandlerPluginInterface[]
      */
     protected function getExceptionHandlerPlugins(): array
     {
         return [
-            new RedirectExceptionHandlerPlugin(),
             new SubRequestExceptionHandlerPlugin(),
         ];
     }
