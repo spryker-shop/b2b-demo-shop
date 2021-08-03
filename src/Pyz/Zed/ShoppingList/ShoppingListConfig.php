98b8458335105ae5974b3f74fb8df0d6645ba928
ihor.herasimenko <ihor.herasimenko@spryker.com>
2021-08-03 12:58:01 +0300

    SUPESC-409: added `ShoppingListConfig`.

---
+++ src/Pyz/Zed/ShoppingList/ShoppingListConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\ShoppingList;
+
+use Spryker\Zed\ShoppingList\ShoppingListConfig as SprykerShoppingListConfig;
+
+class ShoppingListConfig extends SprykerShoppingListConfig
+{
+    /**
+     * @return bool
+     */
+    public function isShoppingListOverviewWithShoppingLists(): bool
+    {
+        return false;
+    }
+}
