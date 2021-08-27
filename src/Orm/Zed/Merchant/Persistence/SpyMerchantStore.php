768f0815c65ad8abdc4c85b4df5843f94c676e3d
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2021-08-27 12:34:10 +0300

    Merge conflict fixes

--- src/Orm/Zed/Merchant/Persistence/SpyMerchantStore.php
+++ src/Orm/Zed/Merchant/Persistence/SpyMerchantStore.php
@@ -5,21 +5,12 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-<<<<<<< HEAD:src/Orm/Zed/SalesReturn/Persistence/SpySalesReturn.php
-namespace Orm\Zed\SalesReturn\Persistence;
-
-use Spryker\Zed\SalesReturn\Persistence\Propel\AbstractSpySalesReturn as BaseSpySalesReturn;
-
-/**
- * Skeleton subclass for representing a row from the 'spy_sales_return' table.
-=======
 namespace Orm\Zed\Merchant\Persistence;
 
 use Spryker\Zed\Merchant\Persistence\Propel\AbstractSpyMerchantStore as BaseSpyMerchantStore;
 
 /**
  * Skeleton subclass for representing a row from the 'spy_merchant_store' table.
->>>>>>> internal/202108.0-release:src/Orm/Zed/Merchant/Persistence/SpyMerchantStore.php
  *
  *
  *
@@ -27,10 +18,6 @@ use Spryker\Zed\Merchant\Persistence\Propel\AbstractSpyMerchantStore as BaseSpyM
  * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  */
-<<<<<<< HEAD:src/Orm/Zed/SalesReturn/Persistence/SpySalesReturn.php
-class SpySalesReturn extends BaseSpySalesReturn
-=======
 class SpyMerchantStore extends BaseSpyMerchantStore
->>>>>>> internal/202108.0-release:src/Orm/Zed/Merchant/Persistence/SpyMerchantStore.php
 {
 }
--- src/Orm/Zed/ProductRelation/Persistence/SpyProductRelationStore.php
+++ src/Orm/Zed/ProductRelation/Persistence/SpyProductRelationStore.php
@@ -5,21 +5,12 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-<<<<<<< HEAD:src/Orm/Zed/ProductLabel/Persistence/SpyProductLabelStore.php
-namespace Orm\Zed\ProductLabel\Persistence;
-
-use Spryker\Zed\ProductLabel\Persistence\Propel\AbstractSpyProductLabelStore as BaseSpyProductLabelStore;
-
-/**
- * Skeleton subclass for representing a row from the 'spy_product_label_store' table.
-=======
 namespace Orm\Zed\ProductRelation\Persistence;
 
 use Spryker\Zed\ProductRelation\Persistence\Propel\AbstractSpyProductRelationStore as BaseSpyProductRelationStore;
 
 /**
  * Skeleton subclass for representing a row from the 'spy_product_relation_store' table.
->>>>>>> internal/202108.0-release:src/Orm/Zed/ProductRelation/Persistence/SpyProductRelationStore.php
  *
  *
  *
@@ -27,10 +18,6 @@ use Spryker\Zed\ProductRelation\Persistence\Propel\AbstractSpyProductRelationSto
  * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  */
-<<<<<<< HEAD:src/Orm/Zed/ProductLabel/Persistence/SpyProductLabelStore.php
-class SpyProductLabelStore extends BaseSpyProductLabelStore
-=======
 class SpyProductRelationStore extends BaseSpyProductRelationStore
->>>>>>> internal/202108.0-release:src/Orm/Zed/ProductRelation/Persistence/SpyProductRelationStore.php
 {
 }
