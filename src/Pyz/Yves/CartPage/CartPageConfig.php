daa7fccaa307f0ba280ac07fac62da735219a556
Olha Livitchuk <olha.livitchuk@spryker.com>
2021-09-08 01:11:54 +0300

    TE-9003 Integrated into B2B

---
+++ src/Pyz/Yves/CartPage/CartPageConfig.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Yves\CartPage;
+
+use SprykerShop\Yves\CartPage\CartPageConfig as SprykerCartPageConfig;
+
+class CartPageConfig extends SprykerCartPageConfig
+{
+    protected const IS_CART_CART_ITEMS_VIA_AJAX_LOAD_ENABLED = true;
+
+    protected const IS_LOADING_UPSELLING_PRODUCTS_VIA_AJAX_ENABLED = true;
+}
