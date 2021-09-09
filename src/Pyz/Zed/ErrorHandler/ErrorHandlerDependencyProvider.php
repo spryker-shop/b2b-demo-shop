876dd9b19192f85ecfdb7baec6990eed2ae95021
dima_tsemma <dmitry.tsemma@spryker.com>
2021-09-09 10:20:56 +0300

    TE-9522: Integrated b2b

---
+++ src/Pyz/Zed/ErrorHandler/ErrorHandlerDependencyProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\ErrorHandler;
+
+use Spryker\Zed\ErrorHandler\Communication\Plugin\ExceptionHandler\SubRequestExceptionHandlerStrategyPlugin;
+use Spryker\Zed\ErrorHandler\ErrorHandlerDependencyProvider as SprykerErrorHandlerDependencyProvider;
+
+class ErrorHandlerDependencyProvider extends SprykerErrorHandlerDependencyProvider
+{
+    /**
+     * @return \Spryker\Zed\ErrorHandlerExtension\Dependency\Plugin\ExceptionHandlerStrategyPluginInterface[]
+     */
+    protected function getExceptionHandlerStrategyPlugins(): array
+    {
+        return [
+            new SubRequestExceptionHandlerStrategyPlugin(),
+        ];
+    }
+}
