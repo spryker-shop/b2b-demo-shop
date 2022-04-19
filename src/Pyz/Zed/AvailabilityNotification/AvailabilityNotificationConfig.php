8a901fb6ec10c67663be3fa09bf66cbe654dddb6
Alexander Kovtun <97117772+KoalaAlexanderKovtun@users.noreply.github.com>
2022-04-19 17:23:46 +0300

    CC-15970: Enable availability notification check product exist (#1516)
    * CC-15970: enable availability notification check product exist
    
    * CC-15970: fixed pull request comments
    
    * CC-15970 Fixed CS
    
    Co-authored-by: Dmitry Lymarenko <dmitry.lymarenko@spryker.com>

---
+++ src/Pyz/Zed/AvailabilityNotification/AvailabilityNotificationConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\AvailabilityNotification;
+
+use Spryker\Zed\AvailabilityNotification\AvailabilityNotificationConfig as SprykerAvailabilityNotificationConfig;
+
+class AvailabilityNotificationConfig extends SprykerAvailabilityNotificationConfig
+{
+    /**
+     * @var bool
+     */
+    protected const AVAILABILITY_NOTIFICATION_CHECK_PRODUCT_EXISTS = true;
+}
