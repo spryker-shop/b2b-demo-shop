c13391fe34c45e971034d663e81d61508f189157
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2022-04-23 12:48:32 +0300

    Added missing files after merge, updated composer.lock

---
+++ tests/PyzTest/Yves/Application/Presentation/Controller/HomepageCest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace PyzTest\Yves\Application\Presentation\Controller;
+
+use PyzTest\Yves\Application\ApplicationPresentationTester;
+use PyzTest\Yves\Application\PageObject\Homepage;
+
+/**
+ * Auto-generated group annotations
+ *
+ * @group PyzTest
+ * @group Yves
+ * @group Application
+ * @group Presentation
+ * @group Controller
+ * @group HomepageCest
+ * Add your own group annotations below this line
+ */
+class HomepageCest
+{
+    /**
+     * @param \PyzTest\Yves\Application\ApplicationPresentationTester $i
+     *
+     * @return void
+     */
+    public function testICanOpenHomepage(ApplicationPresentationTester $i)
+    {
+        $i->wantTo('See that i can open the homepage');
+        $i->amOnPage(Homepage::URL);
+        $i->canSeeElement('[data-qa="page"]');
+    }
+}
---
+++ tests/PyzTest/Yves/Application/_support/ApplicationCommunicationTester.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace PyzTest\Yves\Application;
+
+use Codeception\Actor;
+
+/**
+ * Inherited Methods
+ *
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ *
+ * @SuppressWarnings(PHPMD)
+ */
+class ApplicationCommunicationTester extends Actor
+{
+    use _generated\ApplicationCommunicationTesterActions;
+
+   /**
+    * Define custom actions here
+    */
+}
