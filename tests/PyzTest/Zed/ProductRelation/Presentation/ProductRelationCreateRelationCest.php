8e663a920ecfe88024cb56cc46fca3bad44e6f01
Nikita Peretyatko <peretyatko@logema.org>
2022-07-12 18:18:45 +0300

    CC-12527: replaced test

--- tests/PyzTest/Zed/ProductRelation/Presentation/ProductRelationCreateRelationCest.php
+++ tests/PyzTest/Zed/ProductRelation/Presentation/ProductRelationCreateRelationCest.php
@@ -24,7 +24,6 @@ use Spryker\Shared\ProductRelation\ProductRelationTypes;
 class ProductRelationCreateRelationCest
 {
     /**
-     *
      * @param \PyzTest\Zed\ProductRelation\ProductRelationPresentationTester $i
      *
      * @return void
@@ -38,7 +37,8 @@ class ProductRelationCreateRelationCest
 
         $i->amOnPage(ProductRelationCreatePage::URL);
 
-        $i->fillField('//*[@id="product_relation_productRelationKey"]', uniqid('key-', false));
+        $key = uniqid('key-', false);
+        $i->fillField('//*[@id="product_relation_productRelationKey"]', $key);
         $i->filterProductsByName(ProductRelationCreatePage::PRODUCT_RELATION_PRODUCT_1_NAME);
 
         $i->waitForProcessingIsDone();
@@ -55,7 +55,6 @@ class ProductRelationCreateRelationCest
         );
 
         $i->clickSaveButton();
-        $i->waitForText(ProductRelationCreatePage::MESSAGE_SUCCESS_PRODUCT_RELATION_CREATED);
-        $i->see(ProductRelationCreatePage::MESSAGE_SUCCESS_PRODUCT_RELATION_CREATED);
+        $i->seeInPageSource(sprintf('%s %s', ProductRelationCreatePage::EDIT_PRODUCT_RELATION_TEXT, $key));
     }
 }
