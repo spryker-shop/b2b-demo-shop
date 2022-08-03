211124cd23e6687a295e68d96baa7a0df6b7ad32
Abid Hussain <abid.hussain@metacube.com>
2022-08-03 18:10:00 +0530

    CC-19107: Remove product list from cms block

--- src/Pyz/Zed/CmsBlockGui/CmsBlockGuiDependencyProvider.php
+++ src/Pyz/Zed/CmsBlockGui/CmsBlockGuiDependencyProvider.php
@@ -10,8 +10,6 @@ namespace Pyz\Zed\CmsBlockGui;
 use Spryker\Zed\CmsBlockCategoryConnector\Communication\Plugin\CmsBlockCategoryFormPlugin;
 use Spryker\Zed\CmsBlockCategoryConnector\Communication\Plugin\CmsBlockCategoryListViewPlugin;
 use Spryker\Zed\CmsBlockGui\CmsBlockGuiDependencyProvider as CmsBlockGuiCmsBlockGuiDependencyProvider;
-use Spryker\Zed\CmsBlockProductConnector\Communication\Plugin\CmsBlockProductAbstractFormPlugin;
-use Spryker\Zed\CmsBlockProductConnector\Communication\Plugin\CmsBlockProductAbstractListViewPlugin;
 use Spryker\Zed\ContentGui\Communication\Plugin\CmsBlockGui\HtmlToTwigExpressionsCmsBlockGlossaryBeforeSavePlugin;
 use Spryker\Zed\ContentGui\Communication\Plugin\CmsBlockGui\TwigExpressionsToHtmlCmsBlockGlossaryAfterFindPlugin;
 use Spryker\Zed\Store\Communication\Plugin\Form\StoreRelationToggleFormTypePlugin;
@@ -19,27 +17,25 @@ use Spryker\Zed\Store\Communication\Plugin\Form\StoreRelationToggleFormTypePlugi
 class CmsBlockGuiDependencyProvider extends CmsBlockGuiCmsBlockGuiDependencyProvider
 {
     /**
-     * @return array
+     * @return array<string> $plugins
      */
     protected function getCmsBlockFormPlugins()
     {
         $plugins = parent::getCmsBlockFormPlugins();
         $plugins = array_merge($plugins, [
             new CmsBlockCategoryFormPlugin(),
-            new CmsBlockProductAbstractFormPlugin(),
         ]);
 
         return $plugins;
     }
 
     /**
-     * @return array
+     * @return array<string>
      */
     protected function getCmsBlockViewPlugins()
     {
         return array_merge(parent::getCmsBlockViewPlugins(), [
             new CmsBlockCategoryListViewPlugin(),
-            new CmsBlockProductAbstractListViewPlugin(),
         ]);
     }
 
