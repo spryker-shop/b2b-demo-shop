07a956fc533936c5c0699dfba85cac1a8850df04
alexanderM91 <oleksandr.myrnyi@spryker.com>
2022-08-11 12:14:07 +0200

    TMP fix for wrong condition

--- config/Zed/cronjobs/jenkins.php
+++ config/Zed/cronjobs/jenkins.php
@@ -168,7 +168,7 @@ $jobs[] = [
 ];
 
 /* Message broker */
-if (Config::get(MessageBrokerAwsConstants::SQS_RECEIVER_CONFIG)) {
+if (Config::get(MessageBrokerAwsConstants::SQS_RECEIVER_CONFIG) !== '[]') {
     $jobs[] = [
         'name' => 'message-broker-consume-channels',
         'command' => '$PHP_BIN vendor/bin/console message-broker:consume --time-limit=15',
