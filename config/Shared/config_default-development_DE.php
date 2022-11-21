776a34dd209375d785a16ace7d2e501bd66781b7
Andrey Ilin <andrey.ilin@spryker.com>
2022-11-21 12:47:40 +0400

    SUPESC-664: Adjusted configs for changing timezones by store.

--- config/Shared/config_default-development_DE.php
+++ config/Shared/config_default-development_DE.php
@@ -1,5 +1,8 @@
 <?php
 
 use Spryker\Shared\Propel\PropelConstants;
+use Spryker\Shared\UtilDateTime\UtilDateTimeConstants;
 
 $config[PropelConstants::ZED_DB_DATABASE] = 'DE_development_zed';
+
+$config[UtilDateTimeConstants::DATE_TIME_ZONE] = 'Europe/Berlin';
--- config/Shared/config_default-development_US.php
+++ config/Shared/config_default-development_US.php
@@ -1,5 +1,8 @@
 <?php
 
 use Spryker\Shared\Propel\PropelConstants;
+use Spryker\Shared\UtilDateTime\UtilDateTimeConstants;
 
 $config[PropelConstants::ZED_DB_DATABASE] = 'US_development_zed';
+
+$config[UtilDateTimeConstants::DATE_TIME_ZONE] = 'America/Los_Angeles';
