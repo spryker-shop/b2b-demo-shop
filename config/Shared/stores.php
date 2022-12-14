1f90c665d320d77bd46b163cd7ecc1ae1830234e
Sri Ramya Vadlamudi <sri.vadlamudi@spryker.com>
2022-12-14 10:55:06 +0100

    update store configs and added deploy file

--- config/Shared/stores.php
+++ config/Shared/stores.php
@@ -130,57 +130,57 @@ $stores['DE'] = [
     'currencyIsoCodes' => ['EUR', 'CHF'],
     'queuePools' => [
         'synchronizationPool' => [
-            'AT-connection',
+            // 'AT-connection',
             'DE-connection',
-        ],
-    ],
-    'storesWithSharedPersistence' => ['AT'],
-];
-
-$stores['AT'] = [
-        'storesWithSharedPersistence' => ['DE'],
-    ] + $stores['DE'];
-
-$stores['US'] = [
-    // different contexts
-    'contexts' => [
-        // shared settings for all contexts
-        '*' => [
-            'timezone' => 'America/Los_Angeles',
-            'dateFormat' => [
-                // short date (11.14.12)
-                'short' => 'm/d/Y',
-                // medium Date (Feb 01. 2012)
-                'medium' => 'M d. Y',
-                // date formatted as described in RFC 2822
-                'rfc' => 'r',
-                'datetime' => 'Y-m-d H:i:s',
-            ],
-        ],
-        // settings for contexts (overwrite shared)
-        'yves' => [],
-        'zed' => [
-            'dateFormat' => [
-                // short date (12-28-2012)
-                'short' => 'm-d-Y',
-            ],
-        ],
-    ],
-    'locales' => [
-        // first entry is default
-        'en' => 'en_US',
-    ],
-    // first entry is default
-    'countries' => ['US'],
-    // internal and shop
-    'currencyIsoCode' => 'USD',
-    'currencyIsoCodes' => ['USD'],
-    'queuePools' => [
-        'synchronizationPool' => [
-            'US-connection',
         ],
     ],
     'storesWithSharedPersistence' => [],
 ];
+
+// $stores['AT'] = [
+//         'storesWithSharedPersistence' => ['DE'],
+//     ] + $stores['DE'];
+
+// $stores['US'] = [
+//     // different contexts
+//     'contexts' => [
+//         // shared settings for all contexts
+//         '*' => [
+//             'timezone' => 'America/Los_Angeles',
+//             'dateFormat' => [
+//                 // short date (11.14.12)
+//                 'short' => 'm/d/Y',
+//                 // medium Date (Feb 01. 2012)
+//                 'medium' => 'M d. Y',
+//                 // date formatted as described in RFC 2822
+//                 'rfc' => 'r',
+//                 'datetime' => 'Y-m-d H:i:s',
+//             ],
+//         ],
+//         // settings for contexts (overwrite shared)
+//         'yves' => [],
+//         'zed' => [
+//             'dateFormat' => [
+//                 // short date (12-28-2012)
+//                 'short' => 'm-d-Y',
+//             ],
+//         ],
+//     ],
+//     'locales' => [
+//         // first entry is default
+//         'en' => 'en_US',
+//     ],
+//     // first entry is default
+//     'countries' => ['US'],
+//     // internal and shop
+//     'currencyIsoCode' => 'USD',
+//     'currencyIsoCodes' => ['USD'],
+//     'queuePools' => [
+//         'synchronizationPool' => [
+//             'US-connection',
+//         ],
+//     ],
+//     'storesWithSharedPersistence' => [],
+// ];
 
 return $stores;
