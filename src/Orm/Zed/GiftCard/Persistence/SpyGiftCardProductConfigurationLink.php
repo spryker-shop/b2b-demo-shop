ccf28d5635681a8f3cf64fee2b5231cf6f1a1b65
mstfEmkSpryker <mustafa.emek@spryker.com>
2023-02-02 16:23:54 +0300

    FRW-1195 Run proper:install to fix travis errors

--- src/Orm/Zed/GiftCard/Persistence/SpyGiftCardProductConfigurationLink.php
+++ src/Orm/Zed/GiftCard/Persistence/SpyGiftCardProductConfigurationLink.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\GiftCard\Persistence;
 
 use Spryker\Zed\GiftCard\Persistence\Propel\AbstractSpyGiftCardProductConfigurationLink as BaseSpyGiftCardProductConfigurationLink;
@@ -10,10 +15,9 @@ use Spryker\Zed\GiftCard\Persistence\Propel\AbstractSpyGiftCardProductConfigurat
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  */
 class SpyGiftCardProductConfigurationLink extends BaseSpyGiftCardProductConfigurationLink
 {
-
 }
