38efb2f112a6589932049437546bdd73397c264f
Bohdan Turchyk <bohdan.turchyk@spryker.com>
2023-03-23 14:41:01 +0200

    PBC-392 CS fix

--- src/Orm/Zed/AclEntity/Persistence/SpyAclEntitySegmentQuery.php
+++ src/Orm/Zed/AclEntity/Persistence/SpyAclEntitySegmentQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\AclEntity\Persistence;
 
 use Spryker\Zed\AclEntity\Persistence\Propel\AbstractSpyAclEntitySegmentQuery as BaseSpyAclEntitySegmentQuery;
@@ -10,10 +15,9 @@ use Spryker\Zed\AclEntity\Persistence\Propel\AbstractSpyAclEntitySegmentQuery as
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  */
 class SpyAclEntitySegmentQuery extends BaseSpyAclEntitySegmentQuery
 {
-
 }
