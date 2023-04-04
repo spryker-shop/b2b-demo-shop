4de8f91066eef2124e97cc5ac4bbca00eff33f9f
upgrader <upgrader@spryker.com>
2023-04-04 09:20:57 +0000

    PR is create by the Spryker Upgrader Bot

---
+++ src/Orm/Zed/MerchantStorage/Persistence/SpyMerchantStorage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\MerchantStorage\Persistence;
+
+use Spryker\Zed\MerchantStorage\Persistence\Propel\AbstractSpyMerchantStorage as BaseSpyMerchantStorage;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_merchant_storage' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyMerchantStorage extends BaseSpyMerchantStorage
+{
+
+}
