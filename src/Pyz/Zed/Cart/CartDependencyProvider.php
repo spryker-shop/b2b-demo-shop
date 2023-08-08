bac966483d8554c6ade0d87ee7ba9e0412ed30a4
Artem <artem.siminenko@gmail.com>
2023-08-08 11:10:15 +0200

    CC-20017 add plugin

--- src/Pyz/Zed/Cart/CartDependencyProvider.php
+++ src/Pyz/Zed/Cart/CartDependencyProvider.php
@@ -43,7 +43,7 @@ use Spryker\Zed\ProductCartConnector\Communication\Plugin\ProductUrlItemExpander
 use Spryker\Zed\ProductCartConnector\Communication\Plugin\RemoveInactiveItemsPreReloadPlugin;
 use Spryker\Zed\ProductConfigurationCart\Communication\Plugin\Cart\ProductConfigurationGroupKeyItemExpanderPlugin;
 use Spryker\Zed\ProductDiscontinued\Communication\Plugin\Cart\ProductDiscontinuedCartPreCheckPlugin;
-use Spryker\Zed\ProductImageCartConnector\Communication\Plugin\ProductImageCartPlugin;
+use Spryker\Zed\ProductImageCartConnector\Communication\Plugin\Cart\ProductImageItemExpanderPlugin;
 use Spryker\Zed\ProductList\Communication\Plugin\CartExtension\ProductListRestrictedItemsPreCheckPlugin;
 use Spryker\Zed\ProductList\Communication\Plugin\CartExtension\RemoveRestrictedItemsPreReloadPlugin;
 use Spryker\Zed\ProductMeasurementUnit\Communication\Plugin\Cart\DefaultQuantitySalesUnitItemExpanderPlugin;
@@ -99,7 +99,7 @@ class CartDependencyProvider extends SprykerCartDependencyProvider
             new QuantitySalesUnitGroupKeyItemExpanderPlugin(),
             new CartItemGroupKeyOptionPlugin(),
             new CartItemWithBundleGroupKeyExpanderPlugin(),
-            new ProductImageCartPlugin(),
+            new ProductImageItemExpanderPlugin(),
             new CartGroupPromotionItems(),
             new QuantitySalesUnitItemExpanderPlugin(),
             new ProductPackagingUnitItemExpanderPlugin(), #ProductPackagingUnit
