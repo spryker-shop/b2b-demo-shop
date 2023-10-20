f1010293bfe4dad76f8c4c46dbd76118d068b545
Artem <artem.siminenko@gmail.com>
2023-10-20 13:30:01 +0200

    SUPESC-784 Fixed method documentation block

--- src/Pyz/Zed/Publisher/PublisherDependencyProvider.php
+++ src/Pyz/Zed/Publisher/PublisherDependencyProvider.php
@@ -405,7 +405,7 @@ class PublisherDependencyProvider extends SprykerPublisherDependencyProvider
     }
 
     /**
-     * @return array
+     * @return list<\Spryker\Zed\PublisherExtension\Dependency\Plugin\PublisherPluginInterface>
      */
     public function getProductAbstractPageSearchPlugins(): array
     {
