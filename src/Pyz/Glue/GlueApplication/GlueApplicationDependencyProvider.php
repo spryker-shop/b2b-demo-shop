180d124ccf7370994891f7d2bdfc5af46c1f91ca
Roman Havrylko <roman.havrylko@spryker.com>
2023-10-27 16:59:05 +0200

    CC-31028: revert dependency changes.

--- src/Pyz/Glue/GlueApplication/GlueApplicationDependencyProvider.php
+++ src/Pyz/Glue/GlueApplication/GlueApplicationDependencyProvider.php
@@ -375,6 +375,7 @@ class GlueApplicationDependencyProvider extends SprykerGlueApplicationDependency
         return [
             new SecurityBlockerCustomerRestRequestValidatorPlugin(),
             new SecurityBlockerAgentRestRequestValidatorPlugin(),
+            new AccessTokenRestRequestValidatorPlugin(),
             new AgentAccessTokenRestRequestValidatorPlugin(),
             new SimultaneousAuthenticationRestRequestValidatorPlugin(),
             new CurrencyParameterValidatorPlugin(),
