fd5cb944d51bcb59832eb481cce1f0de0689037c
Burak Kaya <burak.kaya@spryker.com>
2023-12-05 18:06:14 +0300

    fix

--- src/Pyz/Zed/Log/Communication/LogCommunicationFactory.php
+++ src/Pyz/Zed/Log/Communication/LogCommunicationFactory.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace Pyz\Zed\Log;
+namespace Pyz\Zed\Log\Communication;
 
 use Monolog\Handler\HandlerInterface;
 use Monolog\Handler\StreamHandler;
