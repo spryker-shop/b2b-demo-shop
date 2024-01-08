9e4ef4d44e9787148f4a8ea31325881ffe0c94b1
upgrader <upgrader@spryker.com>
2024-01-08 09:32:28 +0000

    Applied release group `Fixed address mappings in checkout, checkout-data endpoints.`, RG link https://api.release.spryker.com/release-group/5227

---
+++ src/Pyz/Glue/CustomersRestApi/CustomersRestApiConfig.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Glue\CustomersRestApi;
+
+use Generated\Shared\Transfer\RestAddressTransfer;
+use Spryker\Glue\CustomersRestApi\CustomersRestApiConfig as SprykerCustomersRestApiConfig;
+
+class CustomersRestApiConfig extends SprykerCustomersRestApiConfig
+{
+    /**
+     * Specification:
+     * - Defines a list of properties in a `RestAddressTransfer` that should trigger validation skip.
+     *
+     * @api
+     *
+     * @return list<string>
+     */
+    public function getBillingAddressFieldsToSkipValidation(): array
+    {
+        return array_merge(parent::getBillingAddressFieldsToSkipValidation(), [
+            RestAddressTransfer::ID_COMPANY_BUSINESS_UNIT_ADDRESS,
+        ]);
+    }
+}
