fac9cdecf9d20f01f7ee00cccee3a1212b9043cd
Svyatoslav Varpikhovsky <varpihovsky@gmail.com>
2024-01-12 09:57:16 +0200

    FRW-6795 Cleanup travis/ci files.

--- config/Shared/ci/ga.backup/configure_postgres.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set unrestricted access to Postgres on Travis
-sudo sed -i 's/md5/trust/g' /etc/postgresql/12/main/pg_hba.conf
-sudo sed -i 's/peer/trust/g' /etc/postgresql/12/main/pg_hba.conf
-
-# Restart Postgres service
-sudo pg_ctlcluster --skip-systemctl-redirect 12 main restart
