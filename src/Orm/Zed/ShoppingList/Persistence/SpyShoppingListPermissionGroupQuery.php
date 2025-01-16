6e46689e64e73bdfd817ca0f201a55bfb2bfb6be
Sebastian Kroll <s.kroll@basecom.de>
2025-01-16 16:17:54 +0100

    sehr sehr grosse Änderung

--- src/Orm/Zed/ShoppingList/Persistence/SpyShoppingListPermissionGroupQuery.php
+++ src/Orm/Zed/ShoppingList/Persistence/SpyShoppingListPermissionGroupQuery.php
@@ -1,9 +1,6 @@
 <?php
 
-/**
- * This file is part of the Spryker Commerce OS.
- * For full license information, please view the LICENSE file that was distributed with this source code.
- */
+
 
 declare(strict_types = 1);
 
