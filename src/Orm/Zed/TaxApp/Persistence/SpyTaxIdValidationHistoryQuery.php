5b702922ddaac9b729f543b6d4431160b0016e27
vol4onok <alexander.veselov@spryker.com>
2025-02-13 12:48:27 +0200

    ACP-4696: integration

--- src/Orm/Zed/TaxApp/Persistence/SpyTaxIdValidationHistoryQuery.php
+++ src/Orm/Zed/TaxApp/Persistence/SpyTaxIdValidationHistoryQuery.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
 namespace Orm\Zed\TaxApp\Persistence;
 
 use Spryker\Zed\TaxApp\Persistence\Propel\AbstractSpyTaxIdValidationHistoryQuery as BaseSpyTaxIdValidationHistoryQuery;
@@ -10,10 +17,9 @@ use Spryker\Zed\TaxApp\Persistence\Propel\AbstractSpyTaxIdValidationHistoryQuery
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  */
 class SpyTaxIdValidationHistoryQuery extends BaseSpyTaxIdValidationHistoryQuery
 {
-
 }
