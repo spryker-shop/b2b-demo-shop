5c3167bf29009b5cf4ae3d34df77fb0697dc4dbe
vol4onok <alexander.veselov@spryker.com>
2025-04-07 14:37:21 +0300

    Integration

--- src/Orm/Zed/ShipmentTypeStorage/Persistence/SpyShipmentTypeListStorageQuery.php
+++ src/Orm/Zed/ShipmentTypeStorage/Persistence/SpyShipmentTypeListStorageQuery.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
 namespace Orm\Zed\ShipmentTypeStorage\Persistence;
 
 use Spryker\Zed\ShipmentTypeStorage\Persistence\Propel\AbstractSpyShipmentTypeListStorageQuery as BaseSpyShipmentTypeListStorageQuery;
@@ -10,10 +17,9 @@ use Spryker\Zed\ShipmentTypeStorage\Persistence\Propel\AbstractSpyShipmentTypeLi
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  */
 class SpyShipmentTypeListStorageQuery extends BaseSpyShipmentTypeListStorageQuery
 {
-
 }
