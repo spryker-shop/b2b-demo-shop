0fb82fc04a56f94f9903c549e140a98a55bab389
upgrader <upgrader@spryker.com>
2025-04-07 10:13:45 +0000

    Applied release group `SUPESC-926 Adjusted reading shipment type uuids on Checkout.`, RG link https://api.release.spryker.com/release-group/5736

---
+++ src/Orm/Zed/ShipmentTypeStorage/Persistence/SpyShipmentTypeListStorageQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\ShipmentTypeStorage\Persistence;
+
+use Spryker\Zed\ShipmentTypeStorage\Persistence\Propel\AbstractSpyShipmentTypeListStorageQuery as BaseSpyShipmentTypeListStorageQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_shipment_type_list_storage' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyShipmentTypeListStorageQuery extends BaseSpyShipmentTypeListStorageQuery
+{
+
+}
