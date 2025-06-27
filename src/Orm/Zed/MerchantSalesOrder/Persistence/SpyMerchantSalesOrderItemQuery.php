9327725482071a96513399e1ae6ed51e66e6d0aa
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2025-06-27 17:33:03 +0300

    SSP-927 Cypress tests WIP

---
+++ src/Orm/Zed/MerchantSalesOrder/Persistence/SpyMerchantSalesOrderItemQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\MerchantSalesOrder\Persistence;
+
+use Spryker\Zed\MerchantSalesOrder\Persistence\Propel\AbstractSpyMerchantSalesOrderItemQuery as BaseSpyMerchantSalesOrderItemQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_merchant_sales_order_item' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyMerchantSalesOrderItemQuery extends BaseSpyMerchantSalesOrderItemQuery
+{
+
+}
