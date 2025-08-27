ecae427cee2d3aead49d01c6eeb5a4a61834fda1
Aleksey Belan <alexey.belan@spryker.com>
2025-08-27 11:37:08 +0300

    SSP-1161: FE compatibility

--- src/Pyz/Yves/SelfServicePortal/Theme/default/components/molecules/asset-finder/asset-finder.ts
+++ src/Pyz/Yves/SelfServicePortal/Theme/default/components/molecules/asset-finder/asset-finder.ts
@@ -3,10 +3,17 @@ import CoreAssetFinder from 'SelfServicePortal/components/molecules/asset-finder
 import { EVENT_CLOSE_POPUP } from 'ShopUi/components/molecules/main-popup/main-popup';
 import debounce from 'lodash-es/debounce';
 
+declare module 'SelfServicePortal/components/molecules/asset-list/asset-list' {
+    export interface AssetEventDetail {
+        compatibilityLabel: string;
+    }
+}
+
 export default class AssetFinder extends CoreAssetFinder {
     protected popup: HTMLElement;
     protected container: HTMLElement;
     protected selectedName: HTMLElement;
+    protected selectedStatus: HTMLElement;
 
     protected mapEvents(): void {
         this.hiddenInput = document.querySelector(`.${this.getAttribute('input-class-name')}`);
@@ -15,6 +22,7 @@ export default class AssetFinder extends CoreAssetFinder {
         this.popup = document.querySelector(`.${this.getAttribute('popup-class-name')}`);
         this.container = document.querySelector(`.${this.getAttribute('container-class-name')}`);
         this.clearElement = document.querySelector(`.${this.getAttribute('clear-class-name')}`);
+        this.selectedStatus = document.querySelector(`.${this.getAttribute('selected-status-class-name')}`);
 
         super.mapEvents();
     }
@@ -23,6 +31,7 @@ export default class AssetFinder extends CoreAssetFinder {
         super.selectAsset(data);
         this.selectedName.textContent = data.name;
         this.selectedAssetText.textContent = data.serial || '';
+        this.selectedStatus.innerHTML = data.compatibilityLabel.trim() || '';
 
         this.container.classList.add(this.selectedClass);
         this.popup.dispatchEvent(new CustomEvent(EVENT_CLOSE_POPUP));
