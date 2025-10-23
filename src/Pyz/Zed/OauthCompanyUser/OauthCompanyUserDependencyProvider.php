a51a6a4dc097356cddbd3087daae2a1c1d1634dd
denyssokolov <denys.sokolov@spryker.com>
2025-10-23 12:55:33 +0200

    CC-36496: Refactor Oauth plugins to use stored permission variants.

--- src/Pyz/Zed/OauthCompanyUser/OauthCompanyUserDependencyProvider.php
+++ src/Pyz/Zed/OauthCompanyUser/OauthCompanyUserDependencyProvider.php
@@ -10,7 +10,7 @@ declare(strict_types = 1);
 namespace Pyz\Zed\OauthCompanyUser;
 
 use Spryker\Zed\OauthCompanyUser\OauthCompanyUserDependencyProvider as SprykerOauthCompanyUserDependencyProvider;
-use Spryker\Zed\OauthPermission\Communication\Plugin\OauthCompanyUser\PermissionOauthCompanyUserIdentifierExpanderPlugin;
+use Spryker\Zed\OauthPermission\Communication\Plugin\OauthCompanyUser\StoredPermissionOauthCompanyUserIdentifierExpanderPlugin;
 
 class OauthCompanyUserDependencyProvider extends SprykerOauthCompanyUserDependencyProvider
 {
@@ -20,7 +20,7 @@ class OauthCompanyUserDependencyProvider extends SprykerOauthCompanyUserDependen
     protected function getOauthCompanyUserIdentifierExpanderPlugins(): array
     {
         return [
-            new PermissionOauthCompanyUserIdentifierExpanderPlugin(),
+            new StoredPermissionOauthCompanyUserIdentifierExpanderPlugin(),
         ];
     }
 }
