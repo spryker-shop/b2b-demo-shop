8d8754bc4e39cb8fac0963217d9f3f2a2571ed35
Aleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2026-01-02 09:25:11 +0100

    Update config_default.php

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -2,7 +2,6 @@
 
 declare(strict_types = 1);
 
-
 use Generated\Shared\Transfer\AddPaymentMethodTransfer;
 use Generated\Shared\Transfer\AddReviewsTransfer;
 use Generated\Shared\Transfer\AppConfigUpdatedTransfer;
